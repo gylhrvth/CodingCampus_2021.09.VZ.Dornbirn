3c981caf603771071694d01b333d4030a1e36ef2
zijad <zijad.prodinger@gmail.com>
2021-09-14 13:57:58 +0200

    rechenaufgabe variabeln

--- CodingCampus/src/Zijad/AAA.java
+++ CodingCampus/src/Zijad/AAA.java
@@ -4,23 +4,33 @@ public class AAA {
 
         public static void main(String[] args) {
 
-            double zahl1 = 7;
-            double zahl2 = 3;
+            byte zahl1 = 3;
+            byte zahl2 = 7;
+            double zahl3 = 3;
+            double zahl4 = 7;
+            short zahl5 = 3;
+            short zahl6 = 7;
+            int zahl7 = 3;
+            int zahl8 = 7;
+            long zahl9 = 3;
+            long zahl10 = 7;
+            float zahl11= 3;
+            float zahl12= 7;
 
-            byte resultat1 = (byte) (zahl1 + zahl2);
-            System.out.println(resultat1);
 
-            short resultat2 = (short) (zahl1 - zahl2);
-            System.out.println(resultat2);
 
-            int resultat3 = (int) (zahl1 * zahl2);
-            System.out.println(resultat3);
+            System.out.println("div 1" + (zahl1 / zahl2));
+            System.out.println("div 2" + (zahl3 / zahl4));
+            System.out.println("div 3" + (zahl5 / zahl6));
+            System.out.println("add 1" + (zahl7+ zahl8));
+            System.out.println("sub 1" + (zahl9-zahl10));
+            System.out.println("div 5" + (zahl11/zahl12));
 
-            double resultat4 = (zahl1 / zahl2);
-            System.out.println(resultat4);
 
-            float resultat5 = (float) (zahl1 % zahl2);
-            System.out.println(resultat5);
+
+            System.exit(0);
+
+
 
 
 
