4c7145f0328022ca0122a185f57af97f7f1c86d6
StefanGalbavi <stefan.galbavi@gmx.net>
2021-09-14 12:00:45 +0200

    neu

---
+++ CodingCampus/src/Stefan/FirstWeek/dreiD.java
@@ -0,0 +1,19 @@
+package Stefan.FirstWeek;
+
+public class dreiD {
+    public static void main(String[] args) {
+
+        System.out.println("     ___");
+        System.out.println("    /  /\\");
+        System.out.println("   /  /::\\");
+        System.out.println("  /  /:/\\:\\");
+        System.out.println(" /  /:/~/::\\");
+        System.out.println("/__/:/ /:/\\:\\");
+        System.out.println("\\  \\:\\/:/__\\/");
+        System.out.println(" \\  \\::/");
+        System.out.println("  \\  \\:\\");
+        System.out.println("   \\  \\:\\");
+        System.out.println("    \\__\\/");
+
+    }
+}
