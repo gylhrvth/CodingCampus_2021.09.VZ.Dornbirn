aedfa82253a295b05cf14da2d8c2089137c7865b
Mahir <mahir-mahir55@hotmail.com>
2021-09-14 14:32:13 +0200

    Datemtypen und Operatoren Aufgabe Elemente der Leibnitz-Rheie

--- CodingCampus/src/Mahir/Main.java
+++ CodingCampus/src/Mahir/Main.java
@@ -18,35 +18,59 @@ public class Main {
         System.out.println("    \\__\\/");
 
 
-        int number1 = 15;
-        int number2 = 7;
+        int intNumber1 = 15;
+        int intNumber2 = 7;
 
-        int ergebnis = number1 * number2;
+        int ergebnisInt = intNumber1 * intNumber2;
 
-        double dNumber3 = 22.5;
-        double dNumber4 = 12.3;
+        double doubleNumber3 = 22.5;
+        double doubleNumber4 = 12.3;
 
-        double ergebnis2 = dNumber3 + dNumber4;
+        double ergebnisDouble = doubleNumber3 + doubleNumber4;
 
         long longNumber1 = 123232321221L;
         long longNumber2 = 321212333131L;
 
-        long ergebnis3 = longNumber2 - longNumber1;
+        long ergebnisLong = longNumber2 - longNumber1;
 
         float floatNumber1 = 19.7F;
         float floatNumber2 = 234.9F;
 
         float ergebnisFloat = floatNumber2 / floatNumber1;
 
+        byte byteNumber1 = 7;
+        byte byteNumber2 = 111;
 
-        byte ergebnisByte = 11 % 98;
+        byte ergebnisByte = (byte) (byteNumber1 % byteNumber2);
+
+        short shortNumber1 = 497;
+        short shortNumber2 = 5784;
+
+        short ergebnisShort = (short) (shortNumber2 - shortNumber1);
 
 
-        System.out.println(ergebnis);
-        System.out.println(ergebnis2);
-        System.out.println(ergebnis3);
+
+        System.out.println(ergebnisInt);
+        System.out.println(ergebnisDouble);
+        System.out.println(ergebnisLong);
         System.out.println(ergebnisFloat);
         System.out.println(ergebnisByte);
+        System.out.println(ergebnisShort);
+
+        double result = 4-(4/3.0)+(4/5.0)-(4/7.0)+(4/9.0);
+        double result1 = 4-(4/3.0)+(4/5.0)-(4/7.0)+(4/9.0)-(4/11.0)+(4/13.0)-(4/15.0)+(4/17.0);
+        double result2 = 4-(4/3.0)+(4/5.0)-(4/7.0)+(4/9.0)-(4/11.0)+(4/13.0)-(4/15.0)+(4/17.0)-(4/19.0)+(4/21.0);
+        System.out.println(result);
+        System.out.println(result1);
+        System.out.println(result2);
+
+
+
+
+
+
+
+
 
 
     }
