4517cf5c48450dbee800bd4e73f0f60f496da6b9
gylhrvth <gylhrvth@gmail.com>
2021-09-14 09:20:51 +0200

    First commit

---
+++ CodingCampus/src/Bartosz/HelloWorld.java
@@ -0,0 +1,7 @@
+package Bartosz;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Bartosz");
+    }
+}
---
+++ CodingCampus/src/Plamena/HelloWorld.java
@@ -0,0 +1,7 @@
+package Plamena;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Plamena");
+    }
+}
