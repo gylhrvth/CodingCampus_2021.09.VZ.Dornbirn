d20fbf42c7f35b8e114c84ace58ec9ec44495ef1
misko111 <misko_111@live.at>
2021-09-14 12:23:04 +0200

    Fehlerkorrektur public

---
+++ CodingCampus/src/Stjepan/AAA.java
@@ -0,0 +1,18 @@
+package Stjepan;
+
+public class AAA {
+        public static void main(String[] args) {
+
+            System.out.println("        ___ ");
+            System.out.println("       /  /\\ ");
+            System.out.println("      /  /::\\");
+            System.out.println("     /  /:/\\:\\");
+            System.out.println("    /  /:/~/::\\");
+            System.out.println("   /__/:/ /:/\\:\\");
+            System.out.println("   \\  +\\:\\/:/__\\/");
+            System.out.println("    \\  \\::/");
+            System.out.println("     \\  \\:\\");
+            System.out.println("      \\  \\:\\");
+            System.out.println("       \\__\\/");
+        }
+}
