2c7e9b041834804188467a8a82f42a1207e50852
ErayB <eray.pamuk@hotmail.com>
2021-09-14 12:29:07 +0200

    Hello World

--- CodingCampus/src/Gyula/HelloWorld.java
+++ CodingCampus/src/Gyula/HelloWorld.java
@@ -1,16 +1,8 @@
 package Gyula;
 
-public class HelloWorld
-{
-    public static void main(String[] args)
-    {
-
+public class HelloWorld {
+    public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Fertig");
-
-
     }
-
-
 }
-
