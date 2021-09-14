26a44677c2626b0827fc203599dda1e339f9ec71
ErayB <eray.pamuk@hotmail.com>
2021-09-14 13:53:58 +0200

    Aritmethic Operators with different Data Types

---
+++ CodingCampus/src/Eray/week1/AritmethicOperatorsWithDataTypes.java
@@ -0,0 +1,134 @@
+package Eray.week1;
+
+public class AritmethicOperatorsWithDataTypes
+{
+    public static void main(String[] args) {
+       //byte Datatype
+
+        System.out.println("Byte Datatype");
+        byte Num1Byte = 5;
+        byte Num2Byte = 5;
+
+        byte Plus = (byte)(Num1Byte + Num2Byte);
+        byte Subtraction = (byte)(Num1Byte - Num2Byte);
+        byte Multiplication = (byte)(Num1Byte * Num2Byte);
+        byte Divide = (byte)(Num1Byte / Num2Byte);
+        byte Modulo = (byte)(Num1Byte % Num2Byte);
+
+        System.out.println(Plus);
+        System.out.println(Subtraction);
+        System.out.println(Multiplication);
+        System.out.println(Divide);
+        System.out.println(Modulo);
+        //End of byte Datatype
+
+
+        System.out.println();
+        System.out.println("Short Datatype");
+
+        //short Datatype
+        short Num1Short = 580;
+        short Num2Short = 364;
+
+        short PlusShort = (short)(Num1Short + Num2Short);
+        short SubtractionShort = (short)(Num1Short - Num2Short);
+        short MultiplicationShort = (short)(Num1Short * Num2Short);
+        short DivideShort = (short)(Num1Short / Num2Short);
+        short ModuloShort = (short)(Num1Short % Num2Short);
+
+        System.out.println(PlusShort);
+        System.out.println(SubtractionShort);
+        System.out.println(MultiplicationShort);
+        System.out.println(DivideShort);
+        System.out.println(ModuloShort);
+        //End of short Datatype
+
+
+        System.out.println();
+        System.out.println("Int Datatype");
+
+        //int Datatype
+        int Num1Int = 8593000;
+        int Num2Int = 260004;
+
+        int PlusInt = (int)(Num1Int + Num2Int);
+        int SubtractionInt = (int)(Num1Int - Num2Int);
+        int MultiplicationInt = (int)(Num1Int * Num2Int);
+        int DivideInt = (int)(Num1Int / Num2Int);
+        int ModuloInt = (int)(Num1Int % Num2Int);
+
+        System.out.println(PlusInt);
+        System.out.println(SubtractionInt);
+        System.out.println(MultiplicationInt);
+        System.out.println(DivideInt);
+        System.out.println(ModuloInt);
+        //End of int Datatype
+
+
+        System.out.println();
+        System.out.println("Long Datatype");
+
+        //long Datatype
+        long Num1long = 347589094;
+        long Num2long = 12849034;
+
+        long  PlusLong = (long )(Num1long + Num2long);
+        long  SubtractionLong = (long )(Num1long - Num2long);
+        long  MultiplicationLong = (long )(Num1long * Num2long);
+        long  DivideLong = (long )(Num1long / Num2long);
+        long  ModuloLong = (long )(Num1long % Num2long);
+
+        System.out.println(PlusLong);
+        System.out.println(SubtractionLong);
+        System.out.println(MultiplicationLong);
+        System.out.println(DivideLong);
+        System.out.println(ModuloLong);
+        //End of long Datatype
+
+
+        System.out.println();
+        System.out.println("Float Datatype");
+
+        //float Datatype
+        float Num1Float = 347.345F;
+        float Num2Float = 128.245F;
+
+        float  PlusFloat = ( float )(Num1Float + Num2Float);
+        float  SubtractionFloat = ( float )(Num1Float - Num2Float);
+        float MultiplicationFloat = ( float )(Num1Float * Num2Float);
+        float  DivideFloat = ( float )(Num1Float / Num2Float);
+        float  ModuloFloat = ( float )(Num1Float % Num2Float);
+
+        System.out.println(PlusFloat);
+        System.out.println(SubtractionFloat);
+        System.out.println(MultiplicationFloat);
+        System.out.println(DivideFloat);
+        System.out.println(ModuloFloat);
+        //End of float Datatype
+
+
+        System.out.println();
+        System.out.println("Double Datatype");
+
+        //double Datatype
+        double Num1Double = 347.345F;
+        double Num2Double = 128.245F;
+
+        double  PlusDouble = ( double )(Num1Double + Num2Double);
+        double  SubtractionDouble = ( double )(Num1Double - Num2Double);
+        double MultiplicationDouble = ( double )(Num1Double * Num2Double);
+        double  DivideDouble = ( double )(Num1Double / Num2Double);
+        double  ModuloDouble = ( double )(Num1Double % Num2Double);
+
+        System.out.println(PlusDouble);
+        System.out.println(SubtractionDouble);
+        System.out.println(MultiplicationDouble);
+        System.out.println(DivideDouble);
+        System.out.println(ModuloDouble);
+        //End of double Datatype
+
+    }
+
+
+
+}
