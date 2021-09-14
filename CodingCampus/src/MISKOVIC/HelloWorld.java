7db9e81a17b5cb0724fc84fcaa64c4740d1c78dc
misko111 <misko_111@live.at>
2021-09-14 11:26:06 +0200

    fehler

--- CodingCampus/src/MISKOVIC/HelloWorld.java
+++ CodingCampus/src/MISKOVIC/HelloWorld.java
@@ -3,7 +3,7 @@ package MISKOVIC;
 public class HelloWorld {
     public static void main(String[] args){
 
-        System.out.println("Helloooooooo World");
+        System.out.println("Hellooooooooo World");
         System.out.println("Fertig");
 
     }
