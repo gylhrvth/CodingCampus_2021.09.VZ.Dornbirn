43eb5a00d9076b1861a84105f3063e850b7dcb3f
ErayB <eray.pamuk@hotmail.com>
2021-09-14 11:18:58 +0200

    Text hinzugefügt!

--- CodingCampus/src/Gyula/HelloWorld.java
+++ CodingCampus/src/Gyula/HelloWorld.java
@@ -1,18 +1,11 @@
 package Gyula;
 
-public class HellooooWorld {
+public class HelloooWorld {
     public static void main(String[] args) {
         double n = 1;
         int counter = 500000;
 
 
-        while (0 <= counter && n <= counter ) {
-            double pi = 4 / n;
-            n += 2;
-            counter++;
-
-
-            System.out.println(pi);
 
         }
     }
