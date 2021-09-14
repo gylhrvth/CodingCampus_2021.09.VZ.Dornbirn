ff69cc3be8c8ee4d74b4b299f1ec60a8d8d919e6
omer <hems6845@icloud.com>
2021-09-14 12:30:03 +0200

    Letter A

---
+++ CodingCampus/src/Oemer/Main.java
@@ -0,0 +1,26 @@
+package Oemer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        boolean useMoney = true;
+        if (useMoney) {
+            System.out.println("useMoney");
+        } else {
+            System.out.println("notMoney");
+        }
+
+        System.out.println("     __");
+        System.out.println("    /  /\\ ");
+        System.out.println("   /  /::\\");
+        System.out.println("  /  /:/\\:\\");
+        System.out.println(" /  /:/~/::\\");
+        System.out.println("/__/:/ /:/\\:\\");
+        System.out.println("\\  \\/:/__\\/");
+        System.out.println(" \\  \\::/");
+        System.out.println("  \\  \\:\\");
+        System.out.println("   \\  \\:\\");
+        System.out.println("    \\__\\/");
+    }
+}
