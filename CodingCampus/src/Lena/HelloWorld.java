2f33a862f7a586c4824f76891869df73109cc096
LenaNatter <lena.natter@gmail.com>
2021-09-14 11:01:23 +0200

    Weitere Ausgabe eingefügt "Fertig"

--- CodingCampus/src/Lena/HelloWorld.java
+++ CodingCampus/src/Lena/HelloWorld.java
@@ -2,6 +2,9 @@ package Lena;
 
 public class HelloWorld {
     public static void main(String[] args) {
+
         System.out.println("Hello Lena");
+        System.out.println("Fertig");
+
     }
 }
