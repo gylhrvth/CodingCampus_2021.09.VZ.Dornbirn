c80c8800a759579133ce5eeb0782d7a8aa56939a
ErayB <eray.pamuk@hotmail.com>
2021-09-14 12:13:05 +0200

    Hello World

--- CodingCampus/src/Gyula/HelloWorld.java
+++ CodingCampus/src/Gyula/HelloWorld.java
@@ -5,33 +5,12 @@ public class HelloWorld
     public static void main(String[] args)
     {
 
-        System.out.println(PiCalculator());
+        System.out.println("Hello World");
+        System.out.println("Fertig");
 
 
     }
 
-    public static double PiCalculator()
-    {
-            double n = 1;
-            int counter = 500000;
-            double pi = 0;
 
-
-
-            for(int x = 0; x<= counter; x++)
-            {
-                if(x%2==0)
-                {
-                    pi = pi+ (1/n);
-                }
-                else
-                {
-                    pi = pi - (1/n);
-                }
-                n=n +2;
-            }
-            pi = pi *4;
-            return pi;
-    }
 }
 
