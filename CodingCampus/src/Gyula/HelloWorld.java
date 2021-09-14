73f93a5060f817c3ba1981483278887294be4a0e
ErayB <eray.pamuk@hotmail.com>
2021-09-14 12:10:26 +0200

    We create a 3D Letter

--- CodingCampus/src/Gyula/HelloWorld.java
+++ CodingCampus/src/Gyula/HelloWorld.java
@@ -1,11 +1,37 @@
 package Gyula;
 
-public class HelloWorld {
-    public static void main(String[] args) {
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
 
-        System.out.println("Hello World");
-        System.out.println("Fertig");
+        System.out.println(PiCalculator());
 
-        }
+
     }
+
+    public static double PiCalculator()
+    {
+            double n = 1;
+            int counter = 500000;
+            double pi = 0;
+
+
+
+            for(int x = 0; x<= counter; x++)
+            {
+                if(x%2==0)
+                {
+                    pi = pi+ (1/n);
+                }
+                else
+                {
+                    pi = pi - (1/n);
+                }
+                n=n +2;
+            }
+            pi = pi *4;
+            return pi;
+    }
+}
 
