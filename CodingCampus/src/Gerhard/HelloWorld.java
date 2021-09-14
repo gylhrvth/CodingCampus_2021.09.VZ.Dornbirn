63b85505f0d918dcafeee785f75e02370484b474
Gerhard <gkleineberg@gmail.com>
2021-09-14 11:08:38 +0200

    UPDATE HELLO WORLD 14.09.2021

--- CodingCampus/src/Gerhard/HelloWorld.java
+++ CodingCampus/src/Gerhard/HelloWorld.java
@@ -2,6 +2,6 @@ package Gerhard;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Helloo Gerhard");
+        System.out.println("Hellooo Gerhard");
     }
 }
