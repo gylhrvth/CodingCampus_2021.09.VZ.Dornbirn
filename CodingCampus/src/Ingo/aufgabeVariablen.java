c537dfb1305107f04420fc7072fd1567d423bb5d
Ingo888 <somaveritas@protonmail.com>
2021-09-14 14:36:25 +0200

    pi

---
+++ CodingCampus/src/Ingo/aufgabeVariablen.java
@@ -0,0 +1,58 @@
+package Ingo;
+
+
+public class aufgabeVariablen {
+    public static void main(String[]args){
+
+        byte numb1 = 1;
+        byte numb2 = 2;
+        short numb3 = 3;
+        short numb4 = 4;
+        int numb5 = 5;
+        int numb6 = 6;
+        long numb7 = 7;
+        long numb8 = 8;
+        float numb9 = 1f;
+        float numb10 = 10.83472684940323f;
+        double numb11 = 11.9898989123123;
+        double numb12 = 12.788888889111;
+
+
+        System.out.println(numb1+numb2);
+        System.out.println(numb1-numb2);
+        System.out.println(numb1*numb2);
+        System.out.println(numb1/numb2);
+        System.out.println(numb1%numb2);
+        System.out.println(numb3+numb4);
+        System.out.println(numb3-numb4);
+        System.out.println(numb3*numb4);
+        System.out.println(numb3/numb4);
+        System.out.println(numb3%numb4);
+        System.out.println(numb5+numb6);
+        System.out.println(numb5-numb6);
+        System.out.println(numb5*numb6);
+        System.out.println(numb5/numb6);
+        System.out.println(numb10+numb11);
+        System.out.println(numb11*numb12);
+        System.out.println(numb11-numb10);
+        
+
+
+        System.out.println(11/4);
+        System.out.println(11.0/4);
+        System.out.println(13.0/5);
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
