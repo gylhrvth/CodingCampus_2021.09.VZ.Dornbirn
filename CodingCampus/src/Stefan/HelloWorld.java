fb2e64f19126e3cede7b7a17d47d3c8a76562833
StefanGalbavi <stefan.galbavi@gmx.net>
2021-09-14 11:28:54 +0200

    TESTEN

--- CodingCampus/src/Stefan/HelloWorld.java
+++ CodingCampus/src/Stefan/HelloWorld.java
@@ -3,7 +3,7 @@ package Stefan;
 public class HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        System.out.println(" Hello World");
         System.out.println("We are the best!!");
     }
 }
