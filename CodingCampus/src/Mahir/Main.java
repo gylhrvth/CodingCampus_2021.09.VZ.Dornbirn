ff3a521f0630e3310eb168ef26082fa0eb7a6dd9
DCV <mahir-mahir55@hotmail.com>
2021-09-14 12:07:35 +0200

    Aufgaben von Tag 2

---
+++ CodingCampus/src/Mahir/Main.java
@@ -0,0 +1,52 @@
+package Mahir;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("     ___");
+        System.out.println("    /  /\\ ");
+        System.out.println("   /  /::\\ ");
+        System.out.println("  /  /:/\\:\\ ");
+        System.out.println(" /  /:/~/::\\ ");
+        System.out.println("/__/:/ /:/\\:\\ ");
+        System.out.println("\\  \\:\\/:/__\\/");
+        System.out.println(" \\  \\::/");
+        System.out.println("  \\  \\:\\ ");
+        System.out.println("   \\  \\:\\ ");
+        System.out.println("    \\__\\/");
+
+        int number1 = 15;
+        int number2 = 7;
+
+        int ergebnis = number1 * number2;
+
+        double dNumber3 = 22.5;
+        double dNumber4 = 12.3;
+
+        double ergebnis2 = dNumber3 + dNumber4;
+
+        long longNumber1 = 123232321221L;
+        long longNumber2 = 321212333131L;
+
+        long ergebnis3 = longNumber2 - longNumber1;
+
+        float floatNumber1 = 19.7F;
+        float floatNumber2 = 234.9F;
+
+        float ergebnisFloat = floatNumber2 / floatNumber1;
+
+
+        byte ergebnisByte = 11 % 98;
+
+
+        System.out.println(ergebnis);
+        System.out.println(ergebnis2);
+        System.out.println(ergebnis3);
+        System.out.println(ergebnisFloat);
+        System.out.println(ergebnisByte);
+
+
+    }
+}
