7f1034c8a9f34ce005a71894c663db98795837e4
DanGo79 <dannygoerke@.com>
2021-09-14 13:07:37 +0200

    Pi

---
+++ CodingCampus/src/Danny/Pi.java
@@ -0,0 +1,30 @@
+package Danny;
+
+public class Pi {
+
+
+public static void main(String[] args) {
+        // write your code here
+
+                int Zahl = 4;
+                double Teiler = 1.0;
+
+                double ErgebnisTeiler = (Zahl/Teiler);
+                double neuerTeiler = Teiler+2;
+
+
+                double Steigerung = neuerTeiler+2;
+
+
+                double Formel1 = (Zahl/Teiler);
+                Teiler = (Teiler+2);
+                double Formel2 = (Formel1-(Zahl/Teiler));
+                Teiler = (Teiler+2);
+                double Formel3 = (Formel2+(Zahl/Teiler));
+
+
+
+
+    System.out.println(Formel3);
+        }
+        }
