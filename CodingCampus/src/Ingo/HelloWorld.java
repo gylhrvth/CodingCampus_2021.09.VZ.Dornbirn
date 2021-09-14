5ecd44df69744f3f6a511bcee2d071ccd409266f
Ingo888 <somaveritas@protonmail.com>
2021-09-14 14:36:25 +0200

    Comment variablen

--- CodingCampus/src/Ingo/HelloWorld.java
+++ CodingCampus/src/Ingo/HelloWorld.java
@@ -14,11 +14,5 @@ public class HelloWorld {
 
     }
 
-public class HelloWorld1 {
-    public static void main(String[] args) {
-        System.out.println("neues HelloWorld1 und ein Commit");
-
-    }
-}
 
 }
---
+++ CodingCampus/src/Ingo/HelloWorld1.java
@@ -0,0 +1,9 @@
+package Ingo;
+
+public class HelloWorld1 {
+    public static void main(String[] args) {
+        System.out.println("neues HelloWorld1 und ein Commit");
+
+    }
+
+}
