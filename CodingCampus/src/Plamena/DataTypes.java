ba7d3376aeb8a7c4e4ee72f32176fff54749385b
plamenabrigova <90683619+plamenabrigova@users.noreply.github.com>
2021-09-14 13:30:03 +0200

    Zahlen und Typen

---
+++ CodingCampus/src/Plamena/DataTypes.java
@@ -0,0 +1,64 @@
+package Plamena;
+
+public class DataTypes {
+
+
+
+        public static void main(String[] args) {
+
+
+            byte byteOne = 1;
+            byte byteTwo = 8;
+
+            System.out.println(byteOne);
+            System.out.println(byteTwo);
+
+            short shortOne = 2;
+            short shortTwo = 5;
+
+            System.out.println(shortOne);
+            System.out.println(shortTwo);
+
+            int intOne = 6;
+            int intTwo = 7;
+
+            System.out.println(intOne);
+            System.out.println(intTwo);
+
+            long longOne = 456548754960L;
+            long longTwo = 84851564884664L;
+
+            System.out.println(longOne);
+            System.out.println(longTwo);
+
+            float floatOne = 1.06F;
+            float floatTwo = 5.59637916F;
+
+            System.out.println(floatOne);
+            System.out.println(floatTwo);
+
+            double doubleOne = 584.4613197946;
+            double doubleTwo = 45.6464923674664679;
+
+            System.out.println(doubleOne);
+            System.out.println(doubleTwo);
+
+            //Rechnungen
+            System.out.println(byteOne+intTwo);
+            System.out.println(floatOne-doubleTwo);
+            System.out.println(shortTwo*floatTwo);
+            System.out.println(shortTwo/doubleOne);
+            System.out.println(doubleTwo/intOne);
+            System.out.println(floatOne/doubleTwo);
+            System.out.println(shortOne/longTwo);
+            System.out.println(intOne%intTwo);
+
+
+
+
+
+
+
+}
+
+}
