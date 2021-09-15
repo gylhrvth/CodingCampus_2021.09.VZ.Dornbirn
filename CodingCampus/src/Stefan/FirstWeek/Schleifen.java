76135157aa5c836ee869eee69fa05e0f4ddc1930
StefanGalbavi <stefan.galbavi@gmx.net>
2021-09-15 14:35:49 +0200

    Schleifen1

--- CodingCampus/src/Stefan/FirstWeek/Schleifen.java
+++ CodingCampus/src/Stefan/FirstWeek/Schleifen.java
@@ -7,12 +7,27 @@ public class Schleifen {
 //            System.out.println(a*b);
 //        }
 
-        for (int m = 0; m < 5; m++) {
-            for (int l = 0; l < 4; l++) {
-                System.out.print("X");
+//        for (int m = 0; m < 5; m++) {
+//            for (int l = 0; l < m; l++) {
+//                System.out.print("X");
+//            }
+//            System.out.println("X");
+//        }
+
+
+        for (int m = 0; m < 6; m++) {
+            for (int l = 0; l < m; l++) {
+                System.out.print("*");
             }
-            System.out.println("X");
+            System.out.println();
+
         }
 
+        for (int j = 4; j > 0; j--) {
+            for (int r = 0; r < j; r++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
