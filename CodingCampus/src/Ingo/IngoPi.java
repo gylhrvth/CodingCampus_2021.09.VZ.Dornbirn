5c0094da2f250dfd52cb2057edec1fdad989aa30
Ingo888 <somaveritas@protonmail.com>
2021-09-15 09:51:40 +0200

    Comment variablen

---
+++ CodingCampus/src/Ingo/IngoPi.java
@@ -0,0 +1,21 @@
+package Ingo;
+
+public class IngoPi {
+    public static void main(String[] args) {
+
+        int a=0;
+        double pi = 0.0;
+        int zaehler = 1;
+
+        while zahler <=100
+        System.out.println(a-b);
+
+
+        pi = (4/1) - (4/3) + (4/5) - (4/7) + (4/9) - (4/11) + (4/13)
+
+
+
+
+
+    }
+}
