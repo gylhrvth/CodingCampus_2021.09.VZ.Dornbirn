4864ffb7914f79a57678e7e3ea753cfc20fb4bec
StefanGalbavi <stefan.galbavi@gmx.net>
2021-09-15 11:17:59 +0200

    Schleifen

---
+++ CodingCampus/src/Stefan/FirstWeek/Schleifen.java
@@ -0,0 +1,18 @@
+package Stefan.FirstWeek;
+
+public class Schleifen {
+
+    public static void main(String[] args) {
+//        for (int a = 5, b = 2;b <= 10; b++){
+//            System.out.println(a*b);
+//        }
+
+        for (int m = 0; m < 5; m++) {
+            for (int l = 0; l < 4; l++) {
+                System.out.print("X");
+            }
+            System.out.println("X");
+        }
+
+    }
+}
