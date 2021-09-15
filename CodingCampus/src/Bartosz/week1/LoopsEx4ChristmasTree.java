ec2830b9db38ae7292928a0e93dc2ad375a81574
Bartosz <B.Michalak@eclipso.at>
2021-09-15 21:40:45 +0200

    I despite loops within loops with every inch of my body.

--- CodingCampus/src/Bartosz/week1/LoopsEx4ChristmasTree.java
+++ CodingCampus/src/Bartosz/week1/LoopsEx4ChristmasTree.java
@@ -3,33 +3,25 @@ package Bartosz.week1;
 public class LoopsEx4ChristmasTree {
     public static void main(String[] args) {
 
-        int counter = 0;
-        int cols = 14;
-        int baum = 1;
-        int x = 1;
+        int treeTrunkHeight = 3;
+        int height = 10;
 
-        while (counter < cols) {
-
-
-            if (counter < 10) {
-                for (int leer = 10; leer >= 0; leer -= 2) {
-                    System.out.print(".");
-
-                    for (; x == 10; x += 2) {
-                        System.out.println("X");
-                    }
-                }
-                System.out.println(".");
-
-
-                counter++;
-                cols --;
+        for (int rows = 1; rows < height; rows++) {         // Solange die Reihenanzahl kleiner als Höhe ist:
+            for (int space = 1; space < (height - rows); space++) {   // Solange der Abstand kleiner ist als
+                System.out.print(" ");
             }
+
+            for (int Tree = 1; Tree <= rows * 2 - 1; Tree++) {  // Solange
+                System.out.print("X");
+            }
+
+            System.out.println();
         }
 
+        for (int l = 0; l < treeTrunkHeight; l++) {
+            System.out.println("       XXX");
+        }
 
     }
-
-
 }
 
