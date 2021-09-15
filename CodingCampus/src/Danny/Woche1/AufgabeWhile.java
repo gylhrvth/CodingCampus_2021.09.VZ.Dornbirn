a62b867521e389b01c7420217aad8c768868cdf5
DanGo79 <dannygoerke@.com>
2021-09-15 11:22:25 +0200

    While

---
+++ CodingCampus/src/Danny/Woche1/AufgabeWhile.java
@@ -0,0 +1,16 @@
+package Danny.Woche1;
+
+public class Aufgabe1_Schleifen {
+    public static void main(String[] args) {
+
+        // AufgabeWhile
+        int i = 5;
+        while (i < 25) {
+            System.out.println("*");
+            i += 5;
+        }
+
+    }
+    }
+
+
