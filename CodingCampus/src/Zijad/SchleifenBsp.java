e0d9532b8e079d7d5492dcd0726d14c9ee07600d
zijad <zijad.prodinger@gmail.com>
2021-09-15 10:41:36 +0200

    for while

---
+++ CodingCampus/src/Zijad/SchleifenBsp.java
@@ -0,0 +1,21 @@
+package Zijad;
+
+public class SchleifenBsp {
+
+    public static void main(String[] args) {
+
+        {
+            for (int i = 5; i <= 20; i += 5)
+                System.out.println("for" + i);
+
+
+            int j = 5;
+            while (j < 25) {
+                j = j + 5;
+                System.out.println("while" + j);
+            }
+
+        }
+    }
+}
+
