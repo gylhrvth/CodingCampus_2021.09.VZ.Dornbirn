6797fbcb4a9a31d53b35665f5340974fa2ec2fb8
StefanGalbavi <stefan.galbavi@gmx.net>
2021-09-16 11:11:23 +0200

    Schleifen Tanne

--- CodingCampus/src/Stefan/FirstWeek/Schleifen.java
+++ CodingCampus/src/Stefan/FirstWeek/Schleifen.java
@@ -2,34 +2,32 @@ package Stefan.FirstWeek;
 
 public class Schleifen {
 
+    //Aufzählungen
+
+
     public static void main(String[] args) {
-//        for (int a = 5, b = 2;b <= 10; b++){
-//            System.out.println(a*b);
-//        }
+        for (int a = 5, b = 2;b <= 10; b++){
+            System.out.println(a*b);
+        }
 
-//        for (int m = 0; m < 5; m++) {
-//            for (int l = 0; l < m; l++) {
-//                System.out.print("X");
+
+
+        //Pfeilspitze Variabel
+//        int width = 10;
+//        int height = 10;
+//
+//        for (int a = 0; a < width; a++) {
+//            for (int b = 0; b < a; b++) {
+//                System.out.print("*");
 //            }
-//            System.out.println("X");
+//            System.out.println();
 //        }
-
-
-        int width = 10;
-        int height = 10;
-
-        for (int a = 0; a < width; a++) {
-            for (int b = 0; b < a; b++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-
-        for (int c = height; c > 0; c--) {
-            for  (int d = 0; d < c; d++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+//
+//        for (int c = height; c > 0; c--) {
+//            for  (int d = 0; d < c; d++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
     }
 }
