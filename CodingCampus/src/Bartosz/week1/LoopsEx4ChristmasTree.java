b3032edd976de97f9c416d095c872f9433c9e080
Bartosz <B.Michalak@eclipso.at>
2021-09-16 10:24:32 +0200

    I despite loops within loops with every inch of my body.

--- CodingCampus/src/Bartosz/week1/LoopsEx4ChristmasTree.java
+++ CodingCampus/src/Bartosz/week1/LoopsEx4ChristmasTree.java
@@ -3,23 +3,35 @@ package Bartosz.week1;
 public class LoopsEx4ChristmasTree {
     public static void main(String[] args) {
 
-        int treeTrunkHeight = 3;
-        int height = 10;
+        int treeTrunkHeight = 5;
+        int height = 17;
 
-        for (int rows = 1; rows < height; rows++) {         // Solange die Reihenanzahl kleiner als Höhe ist:
-            for (int space = 1; space < (height - rows); space++) {   // Solange der Abstand kleiner ist als
+/*        // Baum
+        for (int rows = 1; rows < height; rows++) {         // Solange die Reihenanzahl kleiner als Höhe des Baumes ist:
+            for (int space = 1; space < (height - rows); space++) {   // Solange der Abstand kleiner ist als Höhe - Reihen
                 System.out.print(" ");
             }
 
-            for (int Tree = 1; Tree <= rows * 2 - 1; Tree++) {  // Solange
+            for (int Tree = 1; Tree <= (rows * 2) - 1; Tree++) {  // Solange Zähler kleiner ist als Reihenanzahl * 2 -1
                 System.out.print("X");
             }
 
-            System.out.println();
-        }
+            System.out.println();                               // Zeilenumbruch nach fertiger Reihe}
+            }*/
 
-        for (int l = 0; l < treeTrunkHeight; l++) {
-            System.out.println("       XXX");
+
+        // Baumstamm
+
+        for (int rows = 1; rows < height; rows++) {         // Solange die Reihenanzahl kleiner als Höhe des Baumes ist:
+            for (int space = 1; space < (height - rows); space++) {   // Solange der Abstand kleiner ist als Höhe - Reihen
+                System.out.print(" ");
+            }
+
+            for (int Tree = 1; Tree <= (rows * 2) - 1; Tree++) {  // Solange Zähler kleiner ist als Reihenanzahl * 2 -1
+                System.out.print("X");
+            }
+
+            System.out.println();                               // Zeilenumbruch nach fertiger Reihe}
         }
 
     }
