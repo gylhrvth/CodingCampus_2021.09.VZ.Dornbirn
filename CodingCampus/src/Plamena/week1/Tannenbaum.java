778914e4b061b7e624f278a5434a35e8b981d249
plamenabrigova <90683619+plamenabrigova@users.noreply.github.com>
2021-09-16 15:04:21 +0200

    Tannenbaum fertig

---
+++ CodingCampus/src/Plamena/week1/Tannenbaum.java
@@ -0,0 +1,76 @@
+package Plamena.week1;
+
+public class Tannenbaum {
+    public static void main(String[] args) {
+
+
+        //Tannenbaum
+        int height = 25;
+        int width = (height * 2 - 1);
+        float heightTrunk = height / 4f;
+        float widthTrunk = width / 4f;
+        float distanceLeft = ((width * (1 / 2f)) - widthTrunk / 2f);
+        float heightStand = height / 6f;
+        float widthStand = width / 2.5f;
+
+
+        //Baum
+        for (int i = 0; i <= height; i++) {
+            for (int j = 0; j <= height - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        //Kugeln
+        for (int j = 0; j <= width; ++j) {
+            if (j <= distanceLeft) {
+                if (j % 2 != 0) {
+                    System.out.print("o");
+                } else {
+                    System.out.print(" ");
+                }
+            } else if (j >= distanceLeft && j <= (width - distanceLeft)) {
+                System.out.print("*");
+            }
+            if (j >= (distanceLeft + widthTrunk)) {
+                if (j % 2 != 0) {
+                    System.out.print("o");
+                } else {
+                    System.out.print(" ");
+                }
+
+            }
+        }
+        System.out.println();
+
+
+//Stamm
+
+        for (float i = 0; i <= heightTrunk; i++) {
+            for (float j = 0; j <= distanceLeft; j++) {
+                System.out.print(" ");
+            }
+            for (float j = 0; j <= widthTrunk; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        //Staender
+
+        for (float i = 0; i <= heightStand; i++) {
+            for (float j = 0; j <= ((width - widthStand) / 2); j++) {
+                System.out.print(" ");
+            }
+            for (float j = 0; j <= widthStand; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
