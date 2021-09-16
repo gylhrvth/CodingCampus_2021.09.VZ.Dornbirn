53c76decf8ffaf2bac51f1a0817f9351b50ea128
StefanGalbavi <stefan.galbavi@gmx.net>
2021-09-16 10:23:10 +0200

    Schleifen Tanne

--- CodingCampus/src/Stefan/FirstWeek/Tannenbaum.java
+++ CodingCampus/src/Stefan/FirstWeek/Tannenbaum.java
@@ -4,52 +4,61 @@ public class Tannenbaum {
     public static void main(String[] args) {
 
         //Baumspitze
-        for (int i=1; i<10; i += 2)
-        {
-            for (int k=0; k < (4 - i / 2); k++)
-            {
+        int widthLeerSpitze = 10;
+        int widthAbstandRand = 4;
+
+        for (int i = 1; i < widthLeerSpitze; i += 2) {
+            for (int k = 0; k < (widthAbstandRand - i / 2); k++) {
                 System.out.print(" ");
             }
-            for (int j=0; j<i; j++)
-            {
+            for (int j = 0; j < i; j++) {
                 System.out.print("*");
             }
             System.out.println("");
         }
-        
+
         //Kreise
-        for (int m = 0; m < 9; m++) {
-            System.out.print("O");
+        int behang = 4;
+
+        for (int m = 0; m <= behang; m++) {
+            System.out.print("O ");
         }
         System.out.println("");
 
         //Baumstamm
+        int widthStamm = 3;
+        int widthLeerStamm = 3;
+
         for (int i = 3; i < 4; i++) {
-            for (int k = 0; k < 3; k++) {
+            for (int k = 0; k < widthLeerStamm; k++) {
                 System.out.print(" ");
             }
-            for (int j = 0; j < 3; j++) {
+            for (int j = 0; j < widthStamm; j++) {
                 System.out.print("*");
             }
             System.out.println("");
         }
 
         for (int i = 3; i < 4; i++) {
-            for (int k = 0; k < 3; k++) {
+            for (int k = 0; k < widthLeerStamm; k++) {
                 System.out.print(" ");
             }
-            for (int j = 0; j < 3; j++) {
+            for (int j = 0; j < widthStamm; j++) {
                 System.out.print("*");
             }
             System.out.println("");
         }
 
         //Halterung
-        for (int i = 1; i < 2; i++) {
-            for (int k = 0; k < 2; k++) {
+        int widthFuss = 4;
+        int heightFuss = 1;
+        int widthLeerFuss = 2;
+
+        for (int i = 1; i <= heightFuss; i++) {
+            for (int k = 0; k < widthLeerFuss; k++) {
                 System.out.print(" ");
             }
-            for (int j = 0; j < 5; j++) {
+            for (int j = 0; j <= widthFuss; j++) {
                 System.out.print("*");
             }
             System.out.println("");
