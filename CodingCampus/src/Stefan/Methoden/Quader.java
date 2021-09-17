7293ea98ab39a6c7b36cdbf2367a3757282712be
StefanGalbavi <stefan.galbavi@gmx.net>
2021-09-17 12:05:31 +0200

    Quader Methode

---
+++ CodingCampus/src/Stefan/Methoden/Quader.java
@@ -0,0 +1,22 @@
+package Stefan.Methoden;
+
+public class Quader {
+    public static void main(String[] args) {
+        printStar(5);
+    }
+
+    //Methoden
+
+    public static void printStar(int height) {
+        int width = height;
+        for (int a = 0; a < height; a++) {
+            for (int b = width; b > 0; b--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+
+}
