99faec802475e7982f29b47d090c85a2916857cc
StefanGalbavi <stefan.galbavi@gmx.net>
2021-09-17 10:21:10 +0200

    Schleifen Tanne

--- CodingCampus/src/Stefan/FirstWeek/Tannenbaum.java
+++ CodingCampus/src/Stefan/FirstWeek/Tannenbaum.java
@@ -5,12 +5,6 @@ public class Tannenbaum {
 
         int heightSpitze = 10;
         int widthAbstandRand = heightSpitze;
-        int behang = (heightSpitze * 2 - 1) / 2;
-        int stammAbstand = behang - 2;
-        int widthStamm = heightSpitze / 3;
-        int fussAbstand = behang - 3;
-        int widthFuss = (heightSpitze * 2 - 1) / 4;
-
 
         //Baumspitze
         for (int i = 0; i < heightSpitze; i++) {
@@ -25,6 +19,8 @@ public class Tannenbaum {
 
 
         //Kreise
+        int behang = (heightSpitze * 2 - 1) / 2;
+
         for (int m = 0; m <= behang; m++) {
             System.out.print("O ");
         }
@@ -32,36 +28,32 @@ public class Tannenbaum {
 
 
         //Baumstamm
-        for (int i = 3; i < 4; i++) {
+        int stammAbstand = behang - 2;
+        int widthStamm = heightSpitze / 3;
+        int heightStamm = heightSpitze / 3;
+
+        for (int i = 0; i < heightStamm; i++) {
             for (int k = 0; k < stammAbstand; k++) {
                 System.out.print(" ");
             }
-            for (int j = 0; j < widthStamm; j++) {
+            for (int j = 0; j <= widthStamm; j++) {
                 System.out.print("*");
             }
             System.out.println("");
         }
-
-        for (int i = 3; i < 4; i++) {
-            for (int k = 0; k < stammAbstand; k++) {
-                System.out.print(" ");
-            }
-            for (int j = 0; j < widthStamm; j++) {
-                System.out.print("*");
-            }
-            System.out.println("");
-        }
-
 
         //Fuss
-        for (int i = 0; i < 1; i++) {
-            for (int k = 0; k < fussAbstand; k++) {
-                System.out.print(" ");
-            }
-            for (int j = 0; j <= widthFuss; j++) {
-                System.out.print("*");
-            }
-            System.out.println("");
-        }
+//        int fussAbstand = behang - 3;
+//        int widthFuss = (heightSpitze * 2 - 1) / 4;
+//
+//        for (int i = 0; i < 1; i++) {
+//            for (int k = 0; k < fussAbstand; k++) {
+//                System.out.print(" ");
+//            }
+//            for (int j = 0; j <= widthFuss; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println("");
+//        }
     }
 }
