b67a6389243d322d191c4cb285ce8aa919132642
Bartosz <B.Michalak@eclipso.at>
2021-09-21 10:13:57 +0200

    Calendar Day-Date-Weekend Method

---
+++ CodingCampus/src/Bartosz/week2/calender/dateAndWeekend.java
@@ -0,0 +1,41 @@
+package Bartosz.week2.calender;
+
+public class dateAndWeekend {
+    public static void main(String[] args) {
+
+        int maxDays = 28;
+        int offset = 4;
+
+        for (int counterDays = 1; counterDays <= maxDays + offset; counterDays++) {   // Tagzähler
+
+            if (counterDays - offset > 0) {
+
+                if (counterDays < 10 + offset) {
+                    System.out.print("|  " + (counterDays - offset) + " ");
+
+                } else if (counterDays == maxDays + offset) {
+                    System.out.print("| " + (counterDays - offset) + " |");
+
+                } else {
+                    System.out.print("| " + (counterDays - offset) + " ");
+                }
+
+                if (counterDays % 7 == 0) {
+                    System.out.print("|");
+                    System.out.println();
+                }
+
+            } else {
+                System.out.print("|  " + "  ");
+            }
+
+        }
+
+        int missingSpaces = 7 - ((maxDays + offset) % 7); // Es fehlen X Tage unten.
+
+        for (int i = 0; i < missingSpaces; i++) {
+            System.out.print("    |");
+        }
+
+    }
+}
