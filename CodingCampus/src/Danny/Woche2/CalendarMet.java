365f3c8eddd1434468fde3ee455515170117c884
DanGo79 <dannygoerke@.com>
2021-09-22 14:24:44 +0200

    Aufgabe Kreis

---
+++ CodingCampus/src/Danny/Woche2/CalendarMet.java
@@ -0,0 +1,81 @@
+package Danny.Woche2;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class CalendarMet {
+
+    public static void main(String[] args) {
+
+
+        GregorianCalendar today = new GregorianCalendar(2021, 7, 10); // für aktuelles Datum leer lassen
+        GregorianCalendar firstDayOfMonth = new GregorianCalendar(
+                today.get(Calendar.YEAR),
+                today.get(Calendar.MONTH),
+                1);
+
+        int actualDayOfMonth = today.get(Calendar.DAY_OF_MONTH);
+        int daysPerMonth = today.getActualMaximum(Calendar.DAY_OF_MONTH);
+        int space = firstDayOfMonth.get(Calendar.DAY_OF_WEEK) - 2;
+        if (space < 0) {
+            space += 7;
+        }
+
+
+        weekdays();
+        indentation(space);
+        representDays(daysPerMonth, actualDayOfMonth, space);
+        restFields(space, daysPerMonth);
+
+    }
+
+    public static void weekdays() {
+        System.out.println("|  Mo  |  Di  |  Mi  |  Do  |  Fr  |  Sa  |  So  |");
+
+    }
+
+    public static void indentation(int space) {
+        for (int y = 0; y < space; y++) {
+            System.out.print("|      ");
+        }
+    }
+
+    public static void representDays(int daysPerMonth, int actualDayOfMonth, int space) {
+        int dayCounter = 1;
+        for (int x = 1; x <= daysPerMonth; x++) {
+            boolean isSaturday = (x + space) % 7 == 6;
+            boolean isSunday = (x + space) % 7 == 0;
+            if (x == actualDayOfMonth && isSaturday) {
+                System.out.printf("| WE%2d*", x);
+            } else if (x == actualDayOfMonth && isSunday) {
+                System.out.printf("| WE%2d*", x);
+                System.out.println("|");
+            } else if (x == actualDayOfMonth) {
+                System.out.printf("|  %2d* ", x);
+            } else if (isSaturday) {
+                System.out.printf("| WE%2d ", x);
+            } else if (isSunday) {
+                System.out.printf("| WE%2d ", x);
+                System.out.println("|");
+            } else {
+                System.out.printf("|  %2d  ", x);
+            }
+            dayCounter++;
+            if (dayCounter == 7) {
+                dayCounter = 0;
+            }
+        }
+    }
+
+
+    public static void restFields(int space, int daysPerMonth) {
+        int rest = (space + daysPerMonth) % 7;
+        for (int x = 0; x < 7 - rest; x++) {
+            System.out.print("|      ");
+        }
+        System.out.println("|");
+    }
+
+
+}
+
--- CodingCampus/src/Lukas/week2/day3/LogicPuzzle.java
+++ CodingCampus/src/Lukas/week2/day3/LogicPuzzle.java
@@ -3,11 +3,32 @@ package Lukas.week2.day3;
 public class LogicPuzzle {
     public static void main(String[] args) {
         //crazySumAndProduct(10) (1 + 2 + 3 + 4 + 5) + (6 * 7 * 8 * 9 * 10) = 30255
+
+        //Diese Methode soll eine Obergrenze als Parameter nehmen,
+        //bis inklusive der Hälfte der Obergrenze summieren
+        //ab exklusive der Hälfte der Obergrenze bis inklusive der Obergrenze soll das Produkt gebildet werden
+        //der Rückgabewert soll die Summe plus das Produkt enthalten
+        //crazySumAndProduct
+
+    crazySumAndProduct(10);
+
+
     }
 
-    //Diese Methode soll eine Obergrenze als Parameter nehmen,
-    //bis inklusive der Hälfte der Obergrenze summieren
-    //ab exklusive der Hälte der Obergrenze bis inklusive der Obergrenze soll das Produkt gebildet werden
-    //der Rückgabewert soll die Summe plus das Produkt enthalten
-    //crazySumAndProduct
+    public static void crazySumAndProduct(int upperLimit) {
+
+        int x = 0;
+        long y = 1;
+
+        for (int i = 1; i <= upperLimit / 2; i++) {
+            x += i;
+        }
+        for (int s = (upperLimit / 2) + 1; s <= upperLimit; s++) {
+            y *= s;
+        }
+        long result = (x + y);
+        System.out.println(result);
+    }
 }
+
+
