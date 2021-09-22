2bda81b84da122c028a47e6e582430fd7dbfe79c
Rauanassaf <90680926+Rauanassaf@users.noreply.github.com>
2021-09-22 09:56:28 +0200

    Hello World

--- CodingCampus/src/Rauan/HelloWorld.java
+++ CodingCampus/src/Rauan/HelloWorld.java
@@ -8,13 +8,9 @@ public class HelloWorld {
         System.out.println("Hello World");
 
 
-        System.out.println("| MO | DI | MI | DO | FR | SA | SO |");
-        System.out.print("| ");
-        int maxDays = 31;
-        for (int i = 0; i <= maxDays; i++) {
-            System.out.print(i);
-        }
-        System.out.printf(" %2d" , i);
+
+
+
 
 
     }
