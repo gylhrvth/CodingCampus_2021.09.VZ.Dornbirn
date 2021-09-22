48e7f97eb8fd6cf83ccc96a13119ae2644b28339
ErayB <eray.pamuk@hotmail.com>
2021-09-22 14:01:25 +0200

    Rekursion und Iterativ  Fibonacci Calculator

--- CodingCampus/src/Eray/week2/Fibonacci.java
+++ CodingCampus/src/Eray/week2/Fibonacci.java
@@ -3,12 +3,13 @@ package Eray.week2;
 public class Fibonacci {
     public static void main(String[] args) {
 
-        System.out.println(faktorCalculator(5));
+        System.out.println(rekursionCalculator(5));
+        System.out.println(iterativCalculator(5));
     }
 
 
 
-    public static int faktorCalculator(int faktor) {
+    public static int rekursionCalculator(int faktor) {
 
         // long Version
 //        if(faktor == 1){
@@ -18,7 +19,17 @@ public class Fibonacci {
 //        return faktor * faktorCalculator(faktor -1);
 //
         // short Version
-        return faktor == 1 ? 1 : faktor * faktorCalculator(faktor - 1);
+        return faktor == 1 ? 1 : faktor * rekursionCalculator(faktor - 1);
+
+    }
+
+    public static int iterativCalculator(int faktor){
+        int ergebnis = 1;
+
+        for (int i = 1; i <= faktor ; i++) {
+            ergebnis *= i;
+        }
+        return ergebnis;
 
     }
 
