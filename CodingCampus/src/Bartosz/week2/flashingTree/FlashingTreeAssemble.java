18d118181a883bd2e044ab67f9eba4548e6c1250
Bartosz <B.Michalak@eclipso.at>
2021-09-23 14:08:52 +0200

    Flashing Tree

---
+++ CodingCampus/src/Bartosz/week2/flashingTree/FlashingTreeAssemble.java
@@ -0,0 +1,9 @@
+package Bartosz.week2.flashingTree;
+
+public class FlashingTreeAssemble {
+    public static void assemble(int height) {
+       FlashingTreeHead.printTree(height);
+       FlashingTreeBalls.printBalls(height);
+       FlashingTreeTrunk.printTrunk(height);
+    }
+}
