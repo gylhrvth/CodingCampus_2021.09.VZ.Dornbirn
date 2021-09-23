4284e493fb1f68be6097057b7e498f89daf6e8fa
StefanGalbavi <stefan.galbavi@gmx.net>
2021-09-23 15:02:24 +0200

    Scanner

--- CodingCampus/src/Stefan/SecondWeek/Scanner/ScannerInput.java
+++ CodingCampus/src/Stefan/SecondWeek/Scanner/ScannerInput.java
@@ -1,7 +1,6 @@
 package Stefan.SecondWeek.Scanner;
 
 
-import Eray.week2.MenuMethods.NewChristmasForMenu;
 import Stefan.SecondWeek.Kalender1;
 
 import java.util.Scanner;
@@ -19,6 +18,12 @@ public class ScannerInput {
             System.out.println("4) Print quader");
             System.out.println("5) Exit");
 
+//            System.out.println("Geben Sie eine Zahl ein!!!");
+//            while (!scanner.hasNextInt()) {
+//                System.out.println("Geben Sie eine Zahl ein!!!");
+//                scanner.nextLine();
+//            }
+
             Scanner scanner = new Scanner(System.in);
             int input = scanner.nextInt();
 
@@ -27,7 +32,7 @@ public class ScannerInput {
                 case 1:
                     System.out.println("Höhe eingeben: ");
                     int newheight = scanner.nextInt();
-                    christmasTree.christmasTree(newheight);
+                    ChristmasTree.christmasTree(newheight);
                     System.out.println();
                     break;
 
@@ -56,12 +61,16 @@ public class ScannerInput {
                     break;
             }
 
+            if (input < 5 && input > 0) {
+                System.out.println("Zum neu starten drücken sie die 1");
+                Scanner restartScanner = new Scanner(System.in);
+                int restart = restartScanner.nextInt();
 
-            System.out.println("Zum neu starten drücken sie die 1");
-            Scanner restartScanner = new Scanner(System.in);
-            int restart = restartScanner.nextInt();
+                if (restart != 1) {
+                    run++;
+                }
 
-            if (restart != 1) {
+            } else {
                 run++;
             }
 
