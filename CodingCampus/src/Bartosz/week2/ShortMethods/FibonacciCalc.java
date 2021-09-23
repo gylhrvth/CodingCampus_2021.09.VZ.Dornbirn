6e18ee82070a31b89a97f0a948b25491b00a7235
Bartosz <B.Michalak@eclipso.at>
2021-09-23 08:23:39 +0200

    Fibonacci Formel Interativ

--- CodingCampus/src/Bartosz/week2/ShortMethods/FibonacciCalc.java
+++ CodingCampus/src/Bartosz/week2/ShortMethods/FibonacciCalc.java
@@ -1,11 +1,28 @@
 package Bartosz.week2.ShortMethods;
 
 public class FibonacciCalc {
-    public static int printFibonacci(int repeat) {
-           int result = 0;
-        for (int counter = 1;  counter <= repeat; counter++) {
-            result = result + (repeat - counter);
-        }
-        return result;
+    public static void main(String[] args) {
+        printAddition(2,2);
     }
-}
+
+    public static void printFibonacci(int firstNumber, int secondNumber) {
+        if (secondNumber > 1000) {  //Sobald die Zweite Zahl so oft addiert wurde um mehr als 1000 zu sein
+            return;                 // Ende
+        }
+        int thirdNumber = firstNumber + secondNumber;       //Summe = DritteZahl
+        System.out.print(thirdNumber + " ");
+        printFibonacci(secondNumber, thirdNumber);
+    }
+
+    public static void printAddition(int add1, int add2) {
+       if(add2 > 200) {
+           return;
+       }
+       else {
+           int summe = add1 + add2;
+           System.out.print(summe + " " + add2 + "    ");
+           printFibonacci(add2, summe);
+       }
+    }
+
+    }
