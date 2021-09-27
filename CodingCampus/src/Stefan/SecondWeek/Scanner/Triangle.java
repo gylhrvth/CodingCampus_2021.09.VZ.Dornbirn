3e55ebcbcf34bf135ebd4062a1a6e9d8745843f3
StefanGalbavi <stefan.galbavi@gmx.net>
2021-09-27 08:28:56 +0200

    Scanner

--- CodingCampus/src/Stefan/SecondWeek/Scanner/Triangle.java
+++ CodingCampus/src/Stefan/SecondWeek/Scanner/Triangle.java
@@ -7,23 +7,27 @@ public class Triangle {
     }
 
     //Methoden
-    public static void triangle(int hight) {
-        int upHeight = hight / 2;
-        int lowHight = hight / 2;
-        for (int a = 0; a < upHeight; a++) {
+    public static void triangle(int width) {
+
+
+//        if(upHeight % 2 == 0) {
+//            upHeight++;
+//        }
+
+        for (int a = 0; a < width; a++) {
             for (int b = 0; b < a; b++) {
                 System.out.print("*");
             }
+            if (a>0){
             System.out.println();
+            }
         }
 
-        int lowerHeight = hight - lowHight;
-        for (int i = lowerHeight; i > 0; i--) {
+        for (int i = width; i > 0; i--) {
             for (int a = 0; a < i; a++) {
                 System.out.print("*");
             }
             System.out.println();
         }
-
     }
 }
