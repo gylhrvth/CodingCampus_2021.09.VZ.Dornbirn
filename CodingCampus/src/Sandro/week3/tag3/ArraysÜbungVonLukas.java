2a1963fb38ef67728ecc19d59aed8664b66218fc
Sandro Horvat <sandrohorvat2211@gmail.com>
2021-09-29 13:55:02 +0200

    Sandros Programme1

---
+++ CodingCampus/src/Sandro/week3/tag3/ArraysÜbungVonLukas.java
@@ -0,0 +1,36 @@
+package Sandro.week3.tag3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArraysÜbungVonLukas {
+
+    public static void main(String[] args) {
+
+        //finding the length of the Array studentNames
+
+
+        System.out.println("Enter the length of the array:");
+
+        Scanner scanner = new Scanner(System.in);           // Scanner instanziiert
+
+        int amountstudents = scanner.nextInt();         // Eingabe in der Konsole
+
+        String[] studentsArray = new String[amountstudents];
+
+
+/*        System.out.println("Enter the elements of the array:");
+
+        for (int i = 0; i < amountstudentss; i++) {
+            myArray[i] = scanner.nextLine();
+        }
+
+        System.out.println(Arrays.toString(myArray));
+    }*/
+    }
+}
+
+
+
+
+
