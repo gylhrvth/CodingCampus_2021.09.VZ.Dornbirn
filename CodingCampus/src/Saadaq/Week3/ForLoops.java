f5295bb89fb3931dfc93b7066c8ecc7256fbcdbb
Umadarasulka <vmcelb@gmail.com>
2021-09-30 08:45:18 +0200

    - For loops.

---
+++ CodingCampus/src/Saadaq/Week3/ForLoops.java
@@ -0,0 +1,24 @@
+package Saadaq.Week3;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+public class ForLoops {
+
+
+    public static void main(String[] args) {
+
+        int n = 5;
+        String Ecco = "Hello World!";
+
+        System.out.printf(Ecco.indent(80));
+
+
+
+
+
+
+    }// End of Main
+
+
+}// End of Class
