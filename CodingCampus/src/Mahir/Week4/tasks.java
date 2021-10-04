a43db94637c37f3c568a16c7e6a6983ff394a8bb
Mahir <mahir-mahir55@hotmail.com>
2021-10-04 15:05:24 +0200

    Aufgaben

---
+++ CodingCampus/src/Mahir/Week4/tasks.java
@@ -0,0 +1,16 @@
+package Mahir.Week4;
+
+public class tasks {
+    public static void main(String[] args) {
+
+        //Berechne die summe der  ersten 100 Zahlen
+        int calc = 0;
+        for (int i = 0; i <= 100; i++) {
+            calc += i;
+            System.out.println(calc);
+        }
+        int result = calc;
+        System.out.println(result);
+
+    }
+}
