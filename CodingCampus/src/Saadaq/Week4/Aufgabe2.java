007d84f5f5d4ff744b1612afed4ce9c30868179a
Umadarasulka <vmcelb@gmail.com>
2021-10-09 21:04:03 +0200

    array übung

---
+++ CodingCampus/src/Saadaq/Week4/Aufgabe2.java
@@ -0,0 +1,62 @@
+package Saadaq.Week4;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Aufgabe2 {
+
+
+    public static void main(String[] args) {
+
+
+        //  (2)
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Type here so we can count it. ");
+
+        String lineReader = sc.nextLine();
+
+        System.out.println(lineReader.compareToIgnoreCase(lineReader));
+
+
+
+
+
+
+
+
+
+        //Buchstaben case insensitive Zählen (A zählt für a)
+
+        char[] ch = new char[lineReader.length()];
+
+
+
+
+
+
+
+        //und ausgeben wie oft jeder Buchstabe vorkommt
+
+
+
+        //TIP aus einem Character kann man eine Zahl gewinnen, bzw. man kann diesen als Zahl annehmen
+
+        //3)
+        //Schlaue Menge
+        //Lies dir folgendes durch:
+        //https://www.brandeins.de/magazine/brand-eins-wirtschaftsmagazin/2005/die-mitte/schlaue-menge
+        //Entwickle ein Program welches zuerst fragt, wieviele Teilnehmer beim Expirement mitmachen
+        //erstelle ein Array in der größe der Teilnehmer, der Typ des Arrays soll Float sein.
+        //Frage jeden Teilnehmer nach seiner Schätzung.
+        //Am Ende berechne den Durchschnitt (arithmetisches Mittel) aller Werte und gib diesen aus.
+
+
+
+    }
+
+
+
+}
