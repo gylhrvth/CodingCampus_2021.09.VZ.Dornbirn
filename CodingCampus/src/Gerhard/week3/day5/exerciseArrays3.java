57118679cc1975e4bc92e7e04ca244f34bdd4eb2
Gerhard <gkleineberg@gmail.com>
2021-10-11 10:14:40 +0200

    NEW EXERCISE

---
+++ CodingCampus/src/Gerhard/week3/day5/exerciseArrays3.java
@@ -0,0 +1,87 @@
+package Gerhard.week3.day5;
+
+import java.util.Scanner;
+
+public class exerciseArrays3 {
+
+
+    public static void main(String[] args) {
+    lineInput();
+
+
+    }
+
+    public static String lineInput() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\u001B[36m" + "Bitte geben Sie einen Satz ein: " + "\u001B[0m");
+        String eingabe = scanner.nextLine();
+        System.out.println();
+        System.out.println("\u001B[36m" + "Sie haben den folgenden Satz eingegeben: " + "\u001B[0m");
+        System.out.println(eingabe);
+        return scanner.nextLine();
+
+    }
+
+
+//    public static String firstArray(char[] myCharArray) {
+//
+//
+//    }
+
+
+
+//    public static void String(String[] args) {
+//
+//        char[] myCharArray = lineInput.toCharArray();
+//        System.out.println();
+//        System.out.println("\u001B[36m" + "Inhalt des Arrays: " + "\u001B[0m");
+//        System.out.println(myCharArray);
+//
+//        System.out.println();
+//        System.out.println("\u001B[36m" + "Ausgabe in umgedrehter Reihenfolge: " + "\u001B[0m");
+//        for (int i = myCharArray.length - 1; i >= 0; i--) {
+//            System.out.print(myCharArray[i]);
+//        }
+//        return myCharArray;
+//    }
+
+
+//   String stringTanga = new String(myCharArray);
+    //      System.out.println(stringTanga);
+
+//        for (int j = myCharArray -1; j>=0; j--)
+//            System.out.print(myCharArray[j]);
+//            System.out.println(java.util.Arrays.toString(myCharArray));
+
+
+}
+
+
+//1)
+//Leichte Aufgabe zum Aufwärmen
+
+//Zeile ausgeben
+//Zeile in Character Array verwandeln und umdrehen
+//Umgedrehte Zeile ausgeben
+
+
+//1)
+//Leichte Aufgabe zum Aufwärmen
+//Eine Zeile aus der Kommandozeile einlesen
+//Zeile ausgeben
+//Zeile in Character Array verwandeln und umdrehen
+//Variante a) Direkt umgedreht ausgeben
+//Variante b) Umdrehen mithilfe eines Strings -> String ausgeben
+//Variante c) Umdrehen mithilfe eines neuen Character Arrays -> Character Array in String umwandeln und ausgeben
+//Variante d) Umdrehen im bestehenden Array des Inputs -> Character Array in String umwandeln und ausgeben
+
+
+//2)
+//Text aus Kommandozeile einlesen
+//Buchstaben case insensitive Zählen (A zählt für a)
+//und ausgeben wie oft jeder Buchstabe vorkommt
+//TIP aus einem Character kann man eine Zahl gewinnen, bzw. man kann diesen als Zahl annehmen
+
+
