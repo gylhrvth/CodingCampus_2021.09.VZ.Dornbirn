c9bc6af3699500b0ee7aacfa715b67d7336c38b2
StefanGalbavi <stefan.galbavi@gmx.net>
2021-10-11 13:07:40 +0200

    Test

--- CodingCampus/src/Stefan/WeekFive/actualTimeDate.java
+++ CodingCampus/src/Stefan/WeekFive/actualTimeDate.java
@@ -6,10 +6,10 @@ import java.time.format.DateTimeFormatter;
 public class actualTimeDate {
     public static void main(String[] args) {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-        System.out.println("yyyy/MM/dd HH:mm:ss-> "+dtf.format(LocalDateTime.now()));
+        System.out.println("Aktuelles Datum und Uhrzeit: -> "+dtf.format(LocalDateTime.now()));
 
 
         DateTimeFormatter dtf3 = DateTimeFormatter.ofPattern("yyyy/MMMM/dd HH:mm:ss");
-        System.out.println("yyyy/MMMM/dd HH:mm:ss-> "+dtf3.format(LocalDateTime.now()));
+        System.out.println("Aktuelles Datum und Uhrzeit: -> "+dtf3.format(LocalDateTime.now()));
     }
 }
