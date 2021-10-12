9c776f4d9625e1b8859e20f2e07b3b03f5bda76f
StefanGalbavi <stefan.galbavi@gmx.net>
2021-10-12 15:02:11 +0200

    Test

--- CodingCampus/src/Stefan/WeekFive/textManipulation.java
+++ CodingCampus/src/Stefan/WeekFive/textManipulation.java
@@ -52,7 +52,7 @@ public class textManipulation {
     }
 
     //Zählt die wieviel Zeilen im Text File sind und speichert sie in counLines
-    private static int countLines (String path) {
+    private static int countLines(String path) {
         int countLines = 0;
         try {
             File f = new File(path);
@@ -70,8 +70,32 @@ public class textManipulation {
         return countLines;
     }
 
+
     //Gibt die Text File von der letzten Zeil bis zur ersten aus
-    private static void flipLines (String path) {
+//    private static void flipLines (String path) {
+//        String[] flippedLines = new String[countLines(path)];
+//        try {
+//            File f = new File(path);
+//            Scanner sc = new Scanner(f);
+//
+//            int i = 0;
+//            while (sc.hasNextLine()) {
+//                flippedLines[i] = sc.nextLine();
+//                i++;
+//            }
+//            sc.close();
+//        } catch (FileNotFoundException fnfe) {
+//            fnfe.printStackTrace();
+//        }
+//
+//        for (int i = flippedLines.length - 1; i >= 0 ; i--) {
+//            System.out.println(flippedLines[i]);
+//        }
+//    }
+
+
+    //Gibt die Text File von der rückwärts won der letzten zur ersten Zeile aus
+    private static void flipLines(String path) {
         String[] flippedLines = new String[countLines(path)];
         try {
             File f = new File(path);
@@ -87,9 +111,15 @@ public class textManipulation {
             fnfe.printStackTrace();
         }
 
-        for (int i = flippedLines.length - 1; i >= 0 ; i--) {
-            System.out.println(flippedLines[i]);
+        for (int i = flippedLines.length - 1; i >= 0; i--) {
+            for (int j = flippedLines[i].length() - 1; j >= 0; j--) {
+
+                System.out.print(flippedLines[i].charAt(j));
+            }
+
+            System.out.println();
         }
     }
+
 
 }
