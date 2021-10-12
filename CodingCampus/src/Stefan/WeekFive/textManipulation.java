3c0ccac76ea6b6199dc08e6e994b4a3b911a5041
StefanGalbavi <stefan.galbavi@gmx.net>
2021-10-12 14:21:20 +0200

    Test

--- CodingCampus/src/Stefan/WeekFive/textManipulation.java
+++ CodingCampus/src/Stefan/WeekFive/textManipulation.java
@@ -2,6 +2,7 @@ package Stefan.WeekFive;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class textManipulation {
@@ -9,28 +10,86 @@ public class textManipulation {
 
         String path = "/Users/DCV/CodingCampus_2021.09.VZ.Dornbirn/CodingCampus/src/Stefan/FileReader/textTest";
 
+        flipLines(path);
 
+        //Dreht jede einzelne Zeile um vom Anfang bis Ende
+//        try {
+//            File f = new File(path);
+//            Scanner sc = new Scanner(f);
+//
+//            while (sc.hasNextLine()) {
+//                String line = sc.nextLine();
+//
+//
+//                for (int i = line.length() - 1; i >= 0; i--) {
+//                    System.out.print(line.charAt(i));
+//
+//                }
+//                System.out.println();
+//
+//            }
+//            sc.close();
+//        } catch (FileNotFoundException fnfe) {
+//            fnfe.printStackTrace();
+//        }
+
+
+        //Zählt die wieviel Zeilen im Text File sind
+//        long l = System.currentTimeMillis(); // Systemzeit merken => Dauer berechnen!
+//        try {
+//            BufferedReader in = new BufferedReader(new FileReader(path));
+//            int count = 0;
+//            while ( in.readLine() != null ) {
+//                count++;
+//            }
+//            System.out.println("Anzahl: " + count);
+//            System.out.println("Dauer: " + (System.currentTimeMillis()-l)); // Dauer ausgeben.
+//        }
+//        catch (Exception ex) {
+//            ex.printStackTrace();
+//        }
+
+    }
+
+    //Zählt die wieviel Zeilen im Text File sind und speichert sie in counLines
+    private static int countLines (String path) {
+        int countLines = 0;
         try {
             File f = new File(path);
             Scanner sc = new Scanner(f);
 
             while (sc.hasNextLine()) {
                 String line = sc.nextLine();
-
-
-                for (int i = line.length() - 1; i >= 0; i--) {
-                    System.out.print(line.charAt(i));
-
-                }
-                System.out.println();
+                countLines++;
 
             }
             sc.close();
         } catch (FileNotFoundException fnfe) {
             fnfe.printStackTrace();
         }
-
+        return countLines;
     }
 
+    //Gibt die Text File von der letzten Zeil bis zur ersten aus
+    private static void flipLines (String path) {
+        String[] flippedLines = new String[countLines(path)];
+        try {
+            File f = new File(path);
+            Scanner sc = new Scanner(f);
+
+            int i = 0;
+            while (sc.hasNextLine()) {
+                flippedLines[i] = sc.nextLine();
+                i++;
+            }
+            sc.close();
+        } catch (FileNotFoundException fnfe) {
+            fnfe.printStackTrace();
+        }
+
+        for (int i = flippedLines.length - 1; i >= 0 ; i--) {
+            System.out.println(flippedLines[i]);
+        }
+    }
 
 }
