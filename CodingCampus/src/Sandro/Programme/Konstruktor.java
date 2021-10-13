80f78981573e74d6d6725d2f95fdc6910091003a
Sandro Horvat <sandrohorvat2211@gmail.com>
2021-10-13 11:10:49 +0200

    - Coolness!

--- CodingCampus/src/Sandro/Programme/Konstruktor.java
+++ CodingCampus/src/Sandro/Programme/Konstruktor.java
@@ -7,25 +7,36 @@ public class Konstruktor {
         Hund laprador = new Hund(4, "Laprador", "schwarz", true);
         Hund dackerl = new Hund(9, "Dackel", "braun", false);
         Hund mops = new Hund(3, "Mops", "weiß", true);
-        Hund[] hunde = {laprador, dackerl, mops};
+        Hund schaeferhund = new Hund(4, "Schäferhund", "dunkelbraun", true, true);
+        Hund[] hunde = {laprador, dackerl, mops, schaeferhund};
 
+        // Arten Hunde abfragen
         for (int i = 0; i < hunde.length; i++) {
             System.out.println(hunde[i].art);
         }
         System.out.println();
 
+        // Fellfarbe Hunde abfragen
         for (int i = 0; i < hunde.length; i++) {
             System.out.println(hunde[i].fellfarbe);
         }
 
-
         // Anzahl Hunde (Pfote geben)
-        int counter = 0;
+        int counter1 = 0;
         for (int i = 0; i < hunde.length; i++) {
             if (hunde[i].kannPfotegeben) {
-                counter++;
+                counter1++;
             }
         }
-        System.out.println("\n" + "Wieviele Hunde können Pfote geben? : " + "\n" + counter);
+        System.out.println("\n" + "Wieviele Hunde können Pfote geben? : " + "\n" + counter1);
+
+        // Anzahl Hunde (Kann bellen)
+        int counter2 = 0;
+        for (int i = 0; i < hunde.length; i++) {
+            if (hunde[i].kannbellen) {
+                counter2++;
+            }
+        }
+        System.out.println("\n" + "Wieviele Hunde können bellen? : " + "\n" + counter2);
     }
 }
