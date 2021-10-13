5e0986d61730ccb920fbdfa1ff493c30abe55d0f
StefanGalbavi <stefan.galbavi@gmx.net>
2021-10-13 14:53:00 +0200

    Test

--- CodingCampus/src/Stefan/WeekFive/textManipulation1.java
+++ CodingCampus/src/Stefan/WeekFive/textManipulation1.java
@@ -10,34 +10,78 @@ public class textManipulation1 {
 
         String path = "C:\\Users\\DCV\\CodingCampus_2021.09.VZ.Dornbirn\\CodingCampus\\src\\Stefan\\WeekFive\\file.txt";
 
-        System.out.println(wordCounter(path));
+//        System.out.println(letterCounter(path));
+        String text = readFileToString(path);
+        letterCounter(text);
+
 
     }
 
-
-    private static int wordCounter(String path) {
-
-        int countWords = 0;
+    public static String readFileToString(String path) {
         try {
-            File f = new File(path);
-            Scanner sc = new Scanner(f);
+            File file = new File(path);
+            Scanner scanner = new Scanner(file);
 
-            String counter = "";
-            while (sc.hasNextLine()) {
-                counter += sc.nextLine();
+            String text = "";
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                text += line;
             }
-            sc.close();
-            counter = counter.replaceAll("[\\(\\):,]|\\.[ \n]", " ")
-                    .replaceAll("\n+", " ")
-                    .replaceAll("\s+", " ");
-
-            String[] words = counter.split(" ");
-            countWords = words.length;
-        } catch (FileNotFoundException fnfe) {
-            fnfe.printStackTrace();
+            scanner.close();
+            return text;
+        } catch (FileNotFoundException exc) {
+            exc.printStackTrace();
         }
-        return countWords;
+        return "";
     }
+
+    private static String letterCounter(String param) {
+
+        String neuer = param.toLowerCase();
+        char[] character = neuer.toCharArray();
+        int[] counter = new int[26];
+        System.out.println("Gezählte Buchstaben im Array: ");
+        for (int i = 0; i < character.length; i++) {
+            char myChar = character[i];
+            int indexOfCounter = myChar - 97;
+            if (indexOfCounter >= 0 && indexOfCounter < counter.length) {
+                counter[indexOfCounter]++;
+            }
+        }
+        for (int i = 0; i < counter.length; i++) {
+            if (counter[i] != 0) {
+                System.out.print(("[" + (char) (i + 97)) + "]" + " = " + counter[i] + ", ");
+            }
+        }
+        return neuer;
+    }
+
+
+
+
+//    private static int wordCounter(String path) {
+//
+//        int countWords = 0;
+//        try {
+//            File f = new File(path);
+//            Scanner sc = new Scanner(f);
+//
+//            String counter = "";
+//            while (sc.hasNextLine()) {
+//                counter += sc.nextLine();
+//            }
+//            sc.close();
+//            counter = counter.replaceAll("[\\(\\):,]|\\.[ \n]", " ")
+//                    .replaceAll("\n+", " ")
+//                    .replaceAll("\s+", " ");
+//
+//            String[] words = counter.split(" ");
+//            countWords = words.length;
+//        } catch (FileNotFoundException fnfe) {
+//            fnfe.printStackTrace();
+//        }
+//        return countWords;
+//    }
 
 }
 
