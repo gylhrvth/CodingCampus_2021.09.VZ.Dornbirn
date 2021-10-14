12235e4ecfe2a14dc7af31d47dc211f260fde6c3
Bartosz <B.Michalak@eclipso.at>
2021-10-14 11:18:53 +0200

    String manipulation

--- CodingCampus/src/Bartosz/week5/day3Aufagaben/Init.java
+++
@@ -1,9 +0,0 @@
-package Bartosz.week5.day3Aufagaben;
-
-public class Init {
-    public static void main(){
-        System.out.println("----------AufgabenMenu----------");
-        System.out.println("Grundvoraussetzung ist das importieren des Files um die Aufgaben zu erfüllen.");
-        FileScanner.fileReader();
-    }
-}
