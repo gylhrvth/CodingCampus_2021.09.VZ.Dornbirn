a18d9f9319a431d46adc84f0130a6c672e51adb1
Ingo888 <somaveritas@protonmail.com>
2021-10-18 14:25:00 +0200

    commint text übung

---
+++ CodingCampus/src/Ingo/week5/home/ScannerFile.java
@@ -0,0 +1,15 @@
+package Ingo.week5.home;
+
+import java.util.Scanner;
+
+public class ScannerFile {
+    public static void main(String[] args) {
+
+        String path = "C:\\Users\\DCV\\IdeaProjects\\CodingCampus_2021.09.VZ.Dornbirn\\assets\\file.txt";
+        Scanner scanner = new Scanner(path);
+
+
+
+    }
+
+}
