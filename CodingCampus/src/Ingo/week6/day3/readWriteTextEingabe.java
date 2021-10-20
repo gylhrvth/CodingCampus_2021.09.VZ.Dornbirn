2e306d2609e6a1d9d321ba3049924a622b644152
Ingo888 <somaveritas@protonmail.com>
2021-10-20 11:35:39 +0200

    commint write file

---
+++ CodingCampus/src/Ingo/week6/day3/readWriteTextEingabe.java
@@ -0,0 +1,26 @@
+package Ingo.week6.day3;
+
+import Ingo.week1.IngoChristbaumTag6;
+
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class readWriteTextEingabe {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+
+        try{
+            wr
+            String path = "assets/file.txt";
+            String input = "";
+
+        }catch (FileNotFoundException fnfe){
+            fnfe.printStackTrace();
+        }
+    }
+}
---
+++ CodingCampus/src/Ingo/week6/day3/writeFile.java
@@ -0,0 +1,50 @@
+package Ingo.week6.day3;
+
+import java.io.*;
+
+public class writeFile {
+    public static void main(String[] args) throws IOException {
+        try {
+            String input = readFile("assets/file.txt");
+            writeFile("assets/tmp/output.txt", input);
+            System.out.println(input);
+
+        } catch (FileNotFoundException exc) {
+            exc.printStackTrace();
+        }
+    }
+
+    public static void writeFile(String path, String data) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(path));
+        try {
+            writer.write(data);
+        } finally {
+            try {
+                writer.close();
+            } catch (IOException exc) {
+                //noop
+            }
+        }
+    }
+
+    public static String readFile(String path) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(path));
+        try {
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb
+                        .append(line)
+                        .append(System.lineSeparator());
+            }
+            return sb.toString();
+        } finally {
+            //Tu das immer
+            try {
+                reader.close();
+            } catch (IOException exc) {
+                //Falls exception beim schließen auftritt
+            }
+        }
+    }
+}
