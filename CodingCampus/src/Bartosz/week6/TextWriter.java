bacfad6d39c8fdcbc03da1f5c77fb89de7d06b16
Bartosz <B.Michalak@eclipso.at>
2021-10-20 11:57:06 +0200

    Übungen Lukas

---
+++ CodingCampus/src/Bartosz/week6/TextWriter.java
@@ -0,0 +1,42 @@
+package Bartosz.week6;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class TextWriter {
+    public static void main(String[] args) {
+        try {
+            writeText("C:\\Users\\DCV\\IdeaProjects\\CodingCampus_2021.09.VZ.Dornbirn\\CodingCampus\\src\\Bartosz\\assets\\output.txt");
+        } catch (IOException ecx) {
+            ecx.printStackTrace();
+        }
+    }
+
+    private static void writeText(String path) throws IOException {
+        BufferedWriter ghost = new BufferedWriter(new FileWriter(path));        //Create new Writer form FileWriter of path File
+        Scanner sc = new Scanner(System.in);
+
+        String regbert = "\n";
+        String userInput = sc.nextLine();
+        StringBuilder sb = new StringBuilder();
+        try {
+            if (userInput.compareToIgnoreCase(regbert) >= 0) {
+                sb.append(userInput).append(System.lineSeparator());
+                while(userInput.compareToIgnoreCase(regbert) > 0) {
+                    userInput = sc.nextLine();
+                    sb.append(userInput).append(System.lineSeparator());
+                }
+                ghost.write(sb.toString());
+            }
+        } finally {
+            try {
+                ghost.close();
+            } catch (IOException exc) {
+                // InputOutputException
+            }
+        }
+
+    }
+}
