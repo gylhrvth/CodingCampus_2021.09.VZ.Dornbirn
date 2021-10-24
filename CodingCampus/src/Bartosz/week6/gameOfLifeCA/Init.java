e815e09b333849b8eb91d5da3da58860dd2123ad
Bartosz <B.Michalak@eclipso.at>
2021-10-24 15:21:39 +0200

    Game of life

--- CodingCampus/src/Bartosz/week6/gameOfLifeCA/Init.java
+++ CodingCampus/src/Bartosz/week6/gameOfLifeCA/Init.java
@@ -34,20 +34,46 @@ public class Init {
     public static void printGame(int[][] D2array) {
         int[][] newArray = new int[D2array.length][D2array.length];   // copyArray
         int aliveCounter;
-        for (int i = 1; i < D2array.length - 1; i++) {
+        for (int i = 0; i < D2array.length ; i++) {
             // Row above
 
-            for (int j = 1; j < D2array[i].length - 1; j++) {
+            for (int j = 0; j < D2array[i].length ; j++) {
                 aliveCounter = 0;
 
                 for (int row = -1; row <= 1; row++) {        // Definitions row -> check status Nachbarn row
-                    for (int col = -1; col <= 1; col++) {       // Definitons column -> check status Nachbars index 0/1
-                        if (D2array[i+row][j+col] >= 1) {     // Row above startet bei 1 nicht bei 0 -> deswegen index der überprüften Index und row -1
+                    for (int col = -1; col <= 1; col++) {     // Definitons column -> check status Nachbars index 0/1
+
+                        // Check Border Cases cols
+                        int colToCheck = j + col;
+                        if (j + col >= D2array.length) {
+                            colToCheck = 0;
+                        } else if(j+col == -1) {
+                            colToCheck = D2array.length-1;
+                        }
+                        // Check Border Cases Rows
+                        int rowsToCheck = i + row;
+                        if (i + row >= D2array.length) {
+                            rowsToCheck = 0;
+                        } else if(i + row == -1) {
+                            rowsToCheck = D2array.length-1;
+                        }
+
+                        if (D2array[rowsToCheck][colToCheck] >= 1) {     // Row above startet bei 1 nicht bei 0 -> deswegen index der überprüften Index und row -1
                             aliveCounter++;
                         }
 
                     }
                 }
+
+                // Working code
+//                for (int row = -1; row <= 1; row++) {        // Definitions row -> check status Nachbarn row
+//                    for (int col = -1; col <= 1; col++) {       // Definitons column -> check status Nachbars index 0/1
+//                        if (D2array[i+row][j+col] >= 1) {     // Row above startet bei 1 nicht bei 0 -> deswegen index der überprüften Index und row -1
+//                            aliveCounter++;
+//                        }
+//                    }
+//                }
+
                 aliveCounter -= D2array[i][j];
                 System.out.print(aliveCounter);
 
