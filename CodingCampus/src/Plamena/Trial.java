d3027ae2ae9b805c4c1512253342a77828a29df4
plamenabrigova <90683619+plamenabrigova@users.noreply.github.com>
2021-11-02 14:52:15 +0100

    many changes

--- CodingCampus/src/Plamena/Trial.java
+++ CodingCampus/src/Plamena/Trial.java
@@ -4,8 +4,12 @@ import Plamena.week1.Methods;
 import Plamena.week4.StringExercise;
 
 import java.lang.invoke.MethodHandle;
+import java.util.Arrays;
 import java.util.GregorianCalendar;
+import java.util.Random;
 import java.util.Scanner;
+
+import Plamena.week5.TextExcercise;
 
 public class Trial {
     public static void main(String[] args) {
@@ -23,11 +27,9 @@ public class Trial {
                 System.out.println("Dein Ergebnis ist" + (Zahl1 * Zahl3));
                 break;
         }*/
-
-
-String [] test = {"Sand", "Sonne", "Strand", "Meer", "Coctail", "Strandtuch", "Bar", "Sonnenbrille"};
-
-        StringExercise.printVowelsSingleCount(test);
+        String s = "Hello World";
+        System.out.printf("The String object %s is at hash code %h%n", s, s);
 
     }
+
 }
