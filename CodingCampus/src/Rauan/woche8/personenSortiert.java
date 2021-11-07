79fd3cacef0594f9e41d770ba4ddaec347775ec0
Rauanassaf <90680926+Rauanassaf@users.noreply.github.com>
2021-11-07 20:10:51 +0100

    Merge remote-tracking branch 'origin/main'

---
+++ CodingCampus/src/Rauan/woche8/personenSortiert.java
@@ -0,0 +1,15 @@
+package Rauan.woche8;
+
+import Lukas.week8.day4.MessageType;
+
+public class personenSortiert {
+    public static void main(String[] args) {
+
+
+
+    }
+  private static void printMessage(MessageType pmessagetype){
+
+
+  }
+}
