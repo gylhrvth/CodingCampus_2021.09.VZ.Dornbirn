147159b0af249bf3fcf12256b2b2a60dedb67d9e
Rauanassaf <90680926+Rauanassaf@users.noreply.github.com>
2021-11-11 08:30:36 +0100

    Merge remote-tracking branch 'origin/main'

--- CodingCampus/src/Rauan/Woche9/SimpleCalculator.java
+++ CodingCampus/src/Rauan/Woche9/SimpleCalculator.java
@@ -1,4 +1,60 @@
 package Rauan.Woche9;
 
+import java.util.Scanner;
+
 public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner calc = new Scanner(System.in);
+        int n1=0;
+        int n2=0;
+        double Answer = 0.0;
+        char operator;
+
+        System.out.println("Enter The first Number");
+        n1 = calc.nextInt();
+        System.out.println("Enter The second Number");
+        n2 = calc.nextInt();
+        System.out.println("Select The Order of Operation");
+         operator = calc.next().charAt(0);
+/*
+        if (operator == '+') {
+            Answer = (n1 + n2);
+            System.out.println("Answer:" + Answer);
+        }
+        if (operator == '-') {
+            Answer = (n1 - n2);
+            System.out.println("Answer:" + Answer);
+        }
+        if (operator == '*') {
+            Answer = (n1 * n2);
+            System.out.println("Answer:" + Answer);
+        }
+        if (operator == '/') {
+            Answer = (n1 / n2);
+            System.out.println("Answer:" + Answer);
+        } else {
+            System.out.println("not implemented yet. Sorry!");
+        }
+
+ */
+        switch (operator) {
+            case '+':
+                Answer = n1 + n2;
+                break;
+            case '-':
+                Answer = n1 - n2;
+                break;
+            case '*':
+                Answer = n1 * n2;
+                break;
+            case '/':
+                Answer = n1 / n2;
+                break;
+            default:
+               
+        }
+        System.out.println(n1 + " " + operator + " " + n2+" = "+ Answer);
+
+
+    }
 }
