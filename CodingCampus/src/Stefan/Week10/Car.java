689dea36dcc868db5967acb68eaa052169d2a93c
Stefan Galbavi <stefan.galbavi@gmx.net>
2021-11-14 14:50:50 +0100

    Juchuuuuuuu

--- CodingCampus/src/Stefan/Week10/Car.java
+++ CodingCampus/src/Stefan/Week10/Car.java
@@ -14,4 +14,49 @@ public class Car {
         this.tankCapacity = tankCapacity;
         this.weight = weight;
     }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getKw() {
+        return kw;
+    }
+
+    public void setKw(double kw) {
+        this.kw = kw;
+    }
+
+    public double getTankCapacity() {
+        return tankCapacity;
+    }
+
+    public void setTankCapacity(double tankCapacity) {
+        this.tankCapacity = tankCapacity;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+       return String.format("Hersteller: %2s\nModell: %2s\nkw: %2s\nTankfüllung: %2s\nGewicht: %2s\n", getProducer(), getModel(),getKw(),getTankCapacity(),getWeight());
+    }
 }
