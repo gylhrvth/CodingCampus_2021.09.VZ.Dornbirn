33000ae3dc58cd7363109f9ff884bdce75c61bfa
Rauanassaf <90680926+Rauanassaf@users.noreply.github.com>
2021-11-16 08:19:29 +0100

    Merge remote-tracking branch 'origin/main'

--- CodingCampus/src/Rauan/woche8/Car/Main.java
+++ CodingCampus/src/Rauan/woche8/Car/Main.java
@@ -4,14 +4,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        printAll();
+    }
 
-
-              CarKlasse car = new CarKlasse("Nissan", "Eos 101",
+    static Scanner scanner = new Scanner(System.in);
+    private final static CarKlasse car = new CarKlasse("Nissan", "Eos 101",
             "Benzin", 68.0, 1600, 400);
 
-        System.out.println(car);
 
+    public static void printAll() {
 
 
     }
+
+    public static double driveCarIfTankIsEnough(int kilometer, double sokannesumgehen) {
+        if (kilometer < sokannesumgehen) {
+            System.out.println("Du hast Erfolgreich " + kilometer + "gefahren und du hast noch " + (sokannesumgehen - kilometer) + "Restfahrt");
+        }
+        return sokannesumgehen - kilometer;
+    }
+
+    public static void fillTank(double fehlendeKilometer) {
+        double benzinPreis = 1.47;
+        double maxTank = car.getTankinhalt() * benzinPreis;
+    }
+
+
 }
