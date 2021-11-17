3e29e536af173d40efdb2cf62dcbc930a9eaf944
LenaNatter <lena.natter@gmail.com>
2021-11-17 09:50:39 +0100

    - Lena hat sehr viele Dateien hinzugefügt.

--- CodingCampus/src/Lena/FirstObjects/Filesystem/CountedFileTypes.java
+++ CodingCampus/src/Lena/FirstObjects/Filesystem/CountedFileTypes.java
@@ -1,7 +1,50 @@
 package Lena.FirstObjects.Filesystem;
 
-public class CountedFileTypes {
+import java.io.File;
+import java.security.Key;
+import java.util.HashMap;
+
+public class CountedFileTypes extends FileReceiver {
+
+    private HashMap<String, Integer> countedEndings;
 
 
+    public CountedFileTypes(){
+        setCountedEndings(new HashMap<>());
+    }
+    public void onFileReceived(File file, int depth) {
 
+        if (!file.isDirectory()) {
+            String endig = file.getName().split("\\.")[1];
+
+            if (!countedEndings.containsKey(endig)) {
+                countedEndings.put(endig, 1);
+            } else {
+                countedEndings.put(endig, countedEndings.get(endig) + 1);
+            }
+        }
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (String key : countedEndings.keySet()){
+            sb
+                    .append("Endung: ")
+                    .append(key)
+                    .append(" kam ")
+                    .append(countedEndings.get(key))
+                    .append("mal vor");
+            sb.append(System.lineSeparator());
+        }
+
+        return sb.toString();
+    }
+
+    public HashMap<String, Integer> getCountedEndings() {
+        return countedEndings;
+    }
+
+    public void setCountedEndings(HashMap<String, Integer> countedEndings) {
+        this.countedEndings = countedEndings;
+    }
 }
