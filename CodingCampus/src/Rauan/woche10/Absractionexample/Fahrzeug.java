4eaf920b63e98db9e4616af0b61e238fa0280af1
Rauanassaf <90680926+Rauanassaf@users.noreply.github.com>
2021-11-18 14:35:21 +0100

    Merge remote-tracking branch 'origin/main'

--- CodingCampus/src/Rauan/woche10/Absractionexample/Fahrzeug.java
+++ CodingCampus/src/Rauan/woche10/Absractionexample/Fahrzeug.java
@@ -2,7 +2,13 @@ package Rauan.woche10.Absractionexample;
 
 public abstract class Fahrzeug {
    public void drive(int km){
-
+       start();
+       for (int i = 0; i < km; i++) {
+           if (i % 10 == 0 || i == km - 1) {
+               System.out.println("Drivin kilometer " + i+"->"+(i+1));
+           }
+       }
+       stop();
     }
 
 
