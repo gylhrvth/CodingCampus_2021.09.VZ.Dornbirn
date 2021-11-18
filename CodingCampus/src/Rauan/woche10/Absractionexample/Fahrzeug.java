39abe746430e8049e57bf6d8af2b3297d2ab2366
Rauanassaf <90680926+Rauanassaf@users.noreply.github.com>
2021-11-18 14:19:23 +0100

    Merge remote-tracking branch 'origin/main'

---
+++ CodingCampus/src/Rauan/woche10/Absractionexample/Fahrzeug.java
@@ -0,0 +1,11 @@
+package Rauan.woche10.Absractionexample;
+
+public abstract class Fahrzeug {
+   public void drive(int km){
+
+    }
+
+
+    public abstract void start();
+    public abstract void stop();
+}
---
+++ CodingCampus/src/Rauan/woche10/Absractionexample/Main.java
@@ -0,0 +1,7 @@
+package Rauan.woche10.Absractionexample;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
