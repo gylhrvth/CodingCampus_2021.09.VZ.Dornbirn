a1a5d96a2861a29c67e181917c2bc6f5b0fc393a
Bartosz <B.Michalak@eclipso.at>
2021-11-25 09:01:40 +0100

    Photo memory

---
+++ CodingCampus/src/Bartosz/bank/Database.java
@@ -0,0 +1,16 @@
+package Bartosz.bank;
+
+import java.sql.DriverManager;
+
+public class Database {
+
+
+    public void connect() {
+        //              Method
+        //DriverManager.getConnection("jdbc:mysql://localhost:3306/bank?user");
+    }
+
+    public void disconnect() {
+       // conne
+    }
+}
