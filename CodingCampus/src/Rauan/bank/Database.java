59d94d2e038007cc17ac4d21c2da245360cf1aff
Rauanassaf <90680926+Rauanassaf@users.noreply.github.com>
2021-11-25 11:21:34 +0100

    Merge remote-tracking branch 'origin/main'

---
+++ CodingCampus/src/Rauan/bank/Database.java
@@ -0,0 +1,27 @@
+package Rauan.bank;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+    public Connection getconnection;
+    private String url;
+    private Connection connection;
+
+    public Database() {
+        this("jdbc:mysql://localhost:3306/bank?user=root&password=Rauan1234");
+    }
+
+    public Database(String url) {
+        this.url = url;
+    }
+
+    public void connect() throws SQLException {
+        connection = DriverManager.getConnection(url);
+    }
+
+    public void disconnect() throws SQLException {
+        connection.close();
+    }
+}
