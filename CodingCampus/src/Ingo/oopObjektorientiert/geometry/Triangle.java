2d32b88df556d0ee723534c107cbbca5763669ab
Ingo888 <somaveritas@protonmail.com>
2021-11-25 18:34:51 +0100

    commit sql crud test

---
+++ CodingCampus/src/Ingo/oopObjektorientiert/geometry/Triangle.java
@@ -0,0 +1,16 @@
+package Ingo.oopObjektorientiert.geometry;
+
+public class Triangle extends Geometry{
+
+    int size;
+
+    @Override
+    double getCircumference() {
+        return 0;
+    }
+
+    @Override
+    double getArea() {
+        return 0;
+    }
+}
