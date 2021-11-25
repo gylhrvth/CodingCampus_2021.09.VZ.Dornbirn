9c9beb1b7e7345f25cd8d5d9813b5d5308856c86
lukas <lukas.aichbauer@gmail.com>
2021-11-25 13:26:44 +0100

    - Query user.

--- CodingCampus/test/Lukas/bank/CRUDTest.java
+++ CodingCampus/test/Lukas/bank/CRUDTest.java
@@ -38,6 +38,9 @@ public class CRUDTest {
         Connection connection = database.getConnection();
 
         try {
+            /**
+             * Kunde in Datenbank einfügen
+             */
             String sql = "INSERT INTO kunde(name, adresse, geburtsdatum) VALUES (?, ?, ?);";
 
             PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
@@ -52,16 +55,39 @@ public class CRUDTest {
 
             statement.setDate(3, sqlDate);
 
+            //Hier wird das statement ausgeführt
             int rows = statement.executeUpdate();
 
+            /**
+             * Ergebnis überprüfen
+             */
             Assertions.assertEquals(1, rows);
 
             ResultSet resultSet = statement.getGeneratedKeys();
             if (resultSet.next()) {
-                long kontoNr = resultSet.getLong(1);
-                Assertions.assertTrue(kontoNr > 0);
+                long kundenNr = resultSet.getLong(1);
+                Assertions.assertTrue(kundenNr > 0);
             } else {
                 Assertions.fail("No primary key returned...");
+            }
+
+            String query = "SELECT kundenNr, name, adresse, geburtsdatum FROM kunde";
+            statement = connection.prepareStatement(query);
+            resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                long kundenNr = resultSet.getLong(1);
+                String name = resultSet.getString(2);
+                String adresse = resultSet.getString(3);
+                java.sql.Date geburtsdatum = resultSet.getDate(4);
+                Assertions.assertEquals("Alfons", name);
+                Assertions.assertEquals("Nüziders", adresse);
+                Assertions.assertEquals("2018-01-01", geburtsdatum.toString());
+
+                if(resultSet.next()) {
+                    Assertions.fail("Too many customers...");
+                }
+            } else {
+                Assertions.fail("No customer found...");
             }
         } catch (SQLException exc) {
             Assertions.fail("Could not insert kunde", exc);
