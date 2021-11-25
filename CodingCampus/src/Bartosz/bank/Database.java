186aba21846ce5bf0c43cc121a2b8715433a2b13
Bartosz <B.Michalak@eclipso.at>
2021-11-25 09:30:04 +0100

    Databank connection

--- CodingCampus/src/Bartosz/bank/Database.java
+++ CodingCampus/src/Bartosz/bank/Database.java
@@ -1,16 +1,27 @@
 package Bartosz.bank;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Database {
+    private String url;
+    private Connection connection;
 
-
-    public void connect() {
-        //              Method
-        //DriverManager.getConnection("jdbc:mysql://localhost:3306/bank?user");
+    public Database() {
+        this("jdbc:mysql://localhost:3306/bank?user=root&password=root");
     }
 
-    public void disconnect() {
-       // conne
+    public Database(String url) {
+        this.url = url;
+    }
+
+    public void connect() throws SQLException {
+        //              Method
+        DriverManager.getConnection(url);
+    }
+
+    public void disconnect() throws SQLException {
+        connection.close();
     }
 }
