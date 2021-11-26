58d930dd3f4f22bd9ef5eb700c6d1da6b492618a
Stefan <stefan.galbavi@gmx.net>
2021-11-26 16:42:52 +0100

    updates

--- CodingCampus/src/Stefan/DatenbankTest/DatabaseManager.java
+++ CodingCampus/src/Stefan/DatenbankTest/DatabaseManager.java
@@ -1,10 +1,10 @@
 package Stefan.DatenbankTest;
 
-import java.util.Date;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import Stefan.DatenbankTest.model.Kunde;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DatabaseManager {
     private Database database;
@@ -13,13 +13,13 @@ public class DatabaseManager {
         this.database = database;
     }
 
-    public long createKunde(String name, String adresse, Date geburtsdatum) throws SQLException {
+    public long createKunde(Kunde kunde) throws SQLException {
         String sql = "INSERT INTO kunde(name, adresse, geburtsdatum) VALUES(?, ?, ?)";
         PreparedStatement statement = database.getConnection().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
-        statement.setString(1, name);
-        statement.setString(2, adresse);
-        statement.setDate(3, new java.sql.Date(geburtsdatum.getTime()));
+        statement.setString(1, kunde.getName());
+        statement.setString(2, kunde.getAdress());
+        statement.setDate(3, new java.sql.Date(kunde.getGeburtsdatum().getTime()));
 
         int rows = statement.executeUpdate();
         if (rows > 0) {
@@ -32,5 +32,24 @@ public class DatabaseManager {
         } else {
             return 0;
         }
+    }
+
+    public List<Kunde> readKunde() throws SQLException {
+        String sql = "SELECT kundenNr, name, adresse, geburtsdatum FROM kunde";
+        PreparedStatement statement = database.getConnection().prepareStatement(sql);
+        ResultSet resultSet = statement.executeQuery();
+        List<Kunde> kunden = new ArrayList<>();
+
+        while (resultSet.next()) {
+            long kontoNr = resultSet.getLong(1);
+            String name = resultSet.getString(2);
+            String adresse = resultSet.getString(3);
+            java.sql.Date geburtsdatum = resultSet.getDate(4);
+
+            Kunde kunde = new Kunde(kontoNr, name, adresse, geburtsdatum);
+            kunden.add(kunde);
+        }
+
+        return kunden;
     }
 }
--- CodingCampus/test/Stefan/DatenbankTest/DatabaseManagerTest.java
+++ CodingCampus/test/Stefan/DatenbankTest/DatabaseManagerTest.java
@@ -1,15 +1,17 @@
 package Stefan.DatenbankTest;
 
-import Lena.Calender;
+import Stefan.DatenbankTest.model.Kunde;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 
 public class DatabaseManagerTest {
@@ -19,6 +21,17 @@ public class DatabaseManagerTest {
     public void setUp() throws SQLException {
         database = new Database("jdbc:mysql://localhost:3306/bank3test?user=root&password=%23root%231234");
         database.connect();
+
+        deleteTable("kunde");
+        deleteTable("konto");
+        deleteTable("konto_kunde");
+        deleteTable("transaktion");
+
+    }
+
+    private void deleteTable(String table) throws SQLException {
+        PreparedStatement statement = database.getConnection().prepareStatement("DELETE FROM " + table);
+        statement.executeUpdate();
     }
 
     @AfterEach
@@ -31,12 +44,32 @@ public class DatabaseManagerTest {
         DatabaseManager databaseManager = new DatabaseManager(database);
 
         try {
-            long kontoNr = databaseManager.createKunde("Markus","Kohlendorf", createDate(1980,1,1));
+            long kontoNr = databaseManager.createKunde(new Kunde(0,"Markus","Kohlendorf", createDate(1980,1,1)));
             Assertions.assertTrue(kontoNr > 0);
         } catch (SQLException exc) {
             Assertions.fail("Customer could not be created ...", exc);
         }
 
+    }
+
+    @Test
+    public void testReadKunden() throws SQLException {
+        DatabaseManager databaseManager = new DatabaseManager(database);
+
+        long kundenNr1 = databaseManager.createKunde(new Kunde(0,"Test1","Teststrasse 2", createDate(2001,10,5)));
+        long kundenNr2 = databaseManager.createKunde(new Kunde(0,"Test2","Teststrasse 5", createDate(2010,11,8)));
+        long kundenNr3 = databaseManager.createKunde(new Kunde(0,"Test3","Teststrasse 1", createDate(2011,6,23)));
+        long kundenNr4 = databaseManager.createKunde(new Kunde(0,"Test4","Teststrasse 8", createDate(1901,5,8)));
+
+        List<Kunde> kunden = databaseManager.readKunde();
+        Assertions.assertEquals(4,kunden.size());
+
+        Kunde kunde1 = kunden.get(0);
+
+        Assertions.assertEquals(kundenNr1,kunde1.getKundenNr());
+        Assertions.assertEquals("Test1", kunde1.getName());
+        Assertions.assertEquals("Teststrasse 2", kunde1.getAdress());
+        Assertions.assertEquals(createDate(2001,10,5), kunde1.getGeburtsdatum());
     }
 
     private Date createDate(int year, int month, int day) {
