03bf81be45c10c1b3193e81c7bd6c27506629b82
Rauanassaf <90680926+Rauanassaf@users.noreply.github.com>
2021-11-26 23:35:51 +0100

    Merge remote-tracking branch 'origin/main'

--- CodingCampus/src/Rauan/bank/DatabaseManager.java
+++ CodingCampus/src/Rauan/bank/DatabaseManager.java
@@ -4,21 +4,23 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DatabaseManager {
     private Database database;
 
-    public DatabaseManager (Database database){
-        this.database=database;
+    public DatabaseManager(Database database) {
+        this.database = database;
     }
-    public long creatKunde(String name, String adresse, Date geburtsdatum) throws SQLException {
+
+    public long creatKunde(Kunde kunde) throws SQLException {
         String sql = "Insert into kunde (Name , Adresse, Geburtsdatum) Values(?, ?, ?)...!";
         PreparedStatement statement = database.getconnection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
-        statement.setString(1, name);
-        statement.setString(2, adresse);
-        statement.setString(3, String.valueOf(new java.sql.Date(geburtsdatum.getTime())));
+        statement.setString(1, kunde.getName());
+        statement.setString(2, kunde.getAdresse());
+        statement.setDate(3, new java.sql.Date(kunde.getGeburtsdatum().getTime()));
 
         int rows = statement.executeUpdate();
         if (rows > 0) {
@@ -32,7 +34,26 @@ public class DatabaseManager {
             return 0;
 
 
-    }
+        }
 
     }
+    public List<Kunde>readKunden () throws SQLException {
+        String sql="SELECT kundeNr, name, adresse, Geburtsdatum From Kunde";
+        PreparedStatement statement=database.getconnection.prepareStatement(sql);
+        ResultSet resultSet= statement.executeQuery();
+        List<Kunde> kundes = new ArrayList<>();
+
+        while (resultSet.next()){
+            long kundeNr = resultSet.getLong(1);
+            String name = resultSet.getString(2);
+            String adresse = resultSet.getString(3);
+            java.sql.Date Geburtsdatum = resultSet.getDate(4);
+
+            Kunde kunde = new Kunde(kundeNr,  name, adresse,Geburtsdatum);
+            kundes.add(kunde);
+        }
+        return kundes;
+
+    }
+
 }
