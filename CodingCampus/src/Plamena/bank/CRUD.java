d32d10fed01243634bfcb5fb5896ee6eff6463ba
plamenabrigova <90683619+plamenabrigova@users.noreply.github.com>
2021-12-01 08:34:50 +0100

    ATM started

--- CodingCampus/src/Plamena/bank/CRUD.java
+++ CodingCampus/src/Plamena/bank/CRUD.java
@@ -10,43 +10,35 @@ public class CRUD {
         private Database database;
 
         public void setUp() throws SQLException {
-            database = new Database("jdbc:mysql://localhost:3306/banktest?user=root&password=marsha1l");
+            database = new Database("jdbc:mysql://localhost:3306/bank?user=root&password=marsha1l");
             database.connect();
-            deleteTable("customer");
-            deleteTable("accounts");
-            deleteTable("account_customer");
-            deleteTable("transaction");
-        }
-
-        public void deleteTable(String table) throws SQLException {
-            PreparedStatement statement = database.getConnection().prepareStatement("DELETE FROM " + table);
-            statement.executeUpdate();
         }
 
         public void tearDown() throws SQLException {
             database.disconnect();
         }
 
-        private long createCustomer(String name, String address, Date birthdate) throws SQLException {
+        public void createCustomer(Customer customer) throws SQLException {
             String sql = "INSERT INTO customer(name, address, birthdate) VALUES (?, ?, ?);";
 
             PreparedStatement statement = database.getConnection().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            statement.setString(1, name);
-            statement.setString(2, address);
-            statement.setDate(3, birthdate);
+            statement.setString(1, customer.getName());
+            statement.setString(2, customer.getAddress());
+            statement.setDate(3, new java.sql.Date(customer.getBirthdate().getTime()));
 
+            statement.executeUpdate();
             ResultSet resultSet = statement.getGeneratedKeys();
 
             long customerid = 0;
             if (resultSet.next()) {
                 customerid = resultSet.getLong(1);
+                customer.setCustomerid(customerid);
             } else {
                 System.out.println("No primary key returned...");
             }
-            return customerid;
         }
 
-        private List<Customer> queryCustomers(String whereClause) throws SQLException {
+        public List<Customer> queryCustomers(String whereClause) throws SQLException {
             String query = "SELECT idcustomer, address, birthdate, name FROM customer" + whereClause + ";";
             PreparedStatement statement = database.getConnection().prepareStatement(query);
             ResultSet resultSet = statement.executeQuery();
@@ -72,26 +64,9 @@ public class CRUD {
             return new Date(date.getTime());
         }
 
-
-        public void insertAndSelect() {
-            try {
-                Calendar calendar = Calendar.getInstance();
-                calendar.set(2001, 0, 1, 0, 0, 0);
-                calendar.set(Calendar.MILLISECOND, 0);
-                java.util.Date date = calendar.getTime();
-
-                long customerid = createCustomer("Alfons", "Nüziders", new Date(date.getTime()));
-
-                List<Customer> customers = queryCustomers("");
-
-                for (int i = 0; i < 10; i++) {
-                    createCustomer("Peter" + i, "Bregenzerwald Löwenzahnstraße", getCalendarDate(1990, 2, 3));
-                }
-
-                customers = queryCustomers("");
-                customers = queryCustomers(" WHERE name LIKE 'Pete%'");
-            } catch (SQLException exc) {
-                exc.printStackTrace();
-            }
+        public void deleteCustomer(long idcustomer) throws SQLException {
+            String sql = "DELETE FROM customer WHERE idcustomer = " + idcustomer + ";";
+            PreparedStatement statement = database.getConnection().prepareStatement(sql);
+            statement.executeUpdate();
         }
     }
