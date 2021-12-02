7787dee776f2868e008e10c1f8a31ad1933e6404
Gerhard <gkleineberg@gmail.com>
2021-12-02 11:13:52 +0100

    My changes.

---
+++ CodingCampus/src/Gerhard/week10/selbstKontrolle/UEBUNG3/HouseMain.java
@@ -0,0 +1,44 @@
+package Gerhard.week10.selbstKontrolle.UEBUNG3;
+//
+//import Lukas.week9.day5.Person;
+//
+//import java.util.LinkedList;
+//import java.util.List;
+//import java.util.Scanner;
+//
+//public class HouseMain {
+//
+//    public static void main(String[] args) {
+//
+//
+//        House house1 = new House(3, 9, "Lindenweg 23, 6714 Nüziders");
+//        House house2 = new House(4, 12, "Langacker 23, 6714 Nüziders");
+//        House house3 = new House(1, 6, "Am Ring 34, 1020 Wien");
+//        House house4 = new House(6, 22, "Kornmarkt 34, 6900 Bregenz");
+//
+////        public String toString(){
+////            return String.format("%d/%d/%d", doors, windows, adressen);
+//
+//
+//        //toString();
+//        //   addperson();
+//
+//    }
+//
+////    public static void addperson() {
+////        List<Person> persons = new LinkedList<>();
+////
+////        Scanner scanner = new Scanner(System.in);
+////
+////        System.out.println("Please enter a persons name:");
+////        for (int i = 0; i < 5; i++) {
+////            Person newPerson = new Person(scanner.next());
+////            if (persons.contains(newPerson)) {
+////                System.out.println("Person already exists...");
+////            } else {
+////                persons.add(newPerson);
+////            }
+////        }
+//
+//}
+//
