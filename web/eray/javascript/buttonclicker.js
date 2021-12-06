25751132140d6253994a7d5c448658efa18b8853
ErayB <eray.pamuk@hotmail.com>
2021-12-06 17:56:00 +0100

    HTML CSS JAVASCRIPT

--- web/eray/javascript/buttonclicker.js
+++ web/eray/javascript/buttonclicker.js
@@ -2,11 +2,13 @@
 
 let textElement;
 let textElementFromLocalStorage;
+let textElementFromDateTime;
 let buttonClickedCounter = 0;
 
 window.onload = () => {
     textElement = document.getElementById("text");
     textElementFromLocalStorage = document.getElementById("localStorageId");
+    textElementFromDateTime = document.getElementById("dateTime");
     refreshLocalStorageId();
 
     if (localStorage.getItem("localSavedButtonCounter")) {
@@ -25,5 +27,36 @@ function refreshClickedButton() {
 }
 
 function refreshLocalStorageId() {
-    textElementFromLocalStorage.innerHTML = "On Youre last visit you clicked the Button:<span style='color: red'>" + localStorage.getItem("localSavedButtonCounter") + "</span> Times!";
+    textElementFromLocalStorage.innerHTML = "On Youre last visit you clicked the Button: " + localStorage.getItem("localSavedButtonCounter") + " Times!";
+}
+
+
+function countDateTimes() {
+    var newHtmlElement = document.createElement("li");
+    const date = new Date();
+    newHtmlElement.innerHTML = "Clicked at: " + date.toLocaleDateString() + " " + date.toLocaleTimeString();
+    document.getElementById("list").appendChild(newHtmlElement);
+
+    var newButton = document.createElement("button");
+    newButton.innerHTML = "Remove";
+    document.getElementById("list").appendChild(newButton);
+    newButton.onclick = function () {
+        remove(getIndex(newButton));
+    };
+}
+
+function remove(index) {
+    var list = document.getElementById("list");
+    list.removeChild(list.childNodes[index]);
+    list.removeChild(list.childNodes[index - 1]);
+}
+
+function getIndex(button) {
+    var pressedButtonTimes = button.parentNode.childNodes;
+
+    for (var i = 0; i < pressedButtonTimes.length; i++) {
+        if (pressedButtonTimes[i] == button) {
+            return i;
+        }
+    }
 }
