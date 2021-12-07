06ffe1d76b254ea758eb444fd07d222dc376fddb
Stefan <stefan.galbavi@gmx.net>
2021-12-07 12:06:26 +0100

    tt

--- web/stefan/JavaScriptTut/js/timeStamp.js
+++ web/stefan/JavaScriptTut/js/timeStamp.js
@@ -1,16 +1,35 @@
-window.onload = start;
+let element;
 
-function start() {
-    document.querySelector('#interaktiv').addEventListener('click', klickverarbeitung);
+function timeStamp() {
+    let newElement = document.createElement("li");
+    const d = new Date();
+    newElement.innerHTML =
+        "Clicked at: " + d.toLocaleDateString() + " " + d.toLocaleTimeString();
+    document.getElementById("list").appendChild(newElement);
+
+    let newButton = document.createElement("button");
+    newButton.innerHTML = "Remove";
+    document.getElementById("list").appendChild(newButton);
+    newButton.onclick = function () {
+        remove(getIndex(newButton));
+    };
 }
 
-function klickverarbeitung() {
-    var Zeitstempeltext = document.createTextNode(new Date().toISOString());
-    var TextZuvor = document.createTextNode('Das letzte update: ');
-    var TextDanach = document.createTextNode('\r\n');
 
-    document.querySelector('output').appendChild(TextZuvor);
-    document.querySelector('output').appendChild(Zeitstempeltext);
-    document.querySelector('output').appendChild(TextDanach);
-
+function remove(index) {
+    let list1 = document.getElementById("list");
+    list1.removeChild(list1.childNodes[index]);
+    list1.removeChild(list1.childNodes[index - 1]);
 }
+
+function getIndex(button) {
+    let aElements = button.parentNode.childNodes;
+    let aElementsLength = aElements.length;
+
+    for (var i = 0; i < aElementsLength; i++) {
+        if (aElements[i] == button) {
+            return i;
+        }
+    }
+}
+
