41c647783b1ec0dc42d106d863f6d28bf3e5eeac
Stefan <stefan.galbavi@gmx.net>
2021-12-07 15:45:46 +0100

    tt

--- web/stefan/chuckNorris/index.js
+++ web/stefan/chuckNorris/index.js
@@ -1,14 +1,19 @@
 var witz;
 
-
 async function randomWitze() {
+    let newElement = document.createElement("li");
     let request = await fetch("https://api.chucknorris.io/jokes/random");
     let data = await request.json();
 
-    let element = document.getElementById("text");
+    let element = document.getElementById("textList").appendChild(newElement);
     element.innerHTML = data["value"];
+
 }
 
+function clearSite() {
+    var el = document.getElementById("textList");
+    location.reload();
+}
 
 // function sucheWitz() {
 //     let request = await fetch("https://api.chucknorris.io/jokes/search?query={query}");
