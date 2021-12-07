db71bc3682a5336356fa3c06f3b92bbf83056559
gylhrvth <gylhrvth@gmail.com>
2021-12-07 10:59:16 +0100

    Format with date

--- web/gyula/sandbox/test.js
+++ web/gyula/sandbox/test.js
@@ -10,6 +10,12 @@ function myAppendChild(){
 
   if (inputElement.value != ""){
     let rootElement = document.getElementById("listRoot")
+    let divElementWithDate = document.createElement("div")
+    divElementWithDate.classList.add("divElementWithDate")
+    let dateText = document.createElement("p")
+    dateText.innerHTML = getDateTimeText() 
+    divElementWithDate.appendChild(dateText)
+
     let divElement = document.createElement("div")
     divElement.classList.add("divElement")
 
@@ -22,12 +28,24 @@ function myAppendChild(){
     let buttonElement = document.createElement("button")
     buttonElement.innerHTML = "X"
     buttonElement.addEventListener("click", () => {
-          rootElement.removeChild(divElement)
+          rootElement.removeChild(divElementWithDate)
         }
       )
-    divElement.appendChild(buttonElement)  
-    rootElement.appendChild(divElement)
+    divElement.appendChild(buttonElement)
+    divElementWithDate.appendChild(divElement)  
+    rootElement.appendChild(divElementWithDate)
   } else {
     alert("Please type input first")
   }
+}
+
+function getDateTimeText(){
+  const t = new Date();
+  const date = ('0' + t.getDate()).slice(-2);
+  const month = ('0' + (t.getMonth() + 1)).slice(-2);
+  const year = t.getFullYear();
+  const hours = ('0' + t.getHours()).slice(-2);
+  const minutes = ('0' + t.getMinutes()).slice(-2);
+  const seconds = ('0' + t.getSeconds()).slice(-2);
+  return `${year}.${month}.${date}. ${hours}:${minutes}:${seconds}`;
 }
