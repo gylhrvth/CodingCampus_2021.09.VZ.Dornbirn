0c150e5fccf0e8fdda3977cd0473946d9a184208
DanGO <dannygoerke@icloud,com>
2021-12-08 23:51:30 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	CodingCampus/src/Rauan/bank/DatabaseManager.java

--- web/danny/javascript/clickAddElement/clickAdd.js
+++ web/danny/javascript/clickAddElement/clickAdd.js
@@ -5,21 +5,22 @@ function clickedAdd() {
     const d = new Date();
     let nListe = document.getElementById("list");
     let newNode = document.createElement('li');
-    let spanNode = document.createElement('span')
 
     let paragraphNode = document.createElement('p')
-    paragraphNode.innerHTML = "Clicked Add: " + d.toISOString();
-    spanNode.appendChild(paragraphNode)
+
+    let spanNode = document.createElement('span')
+    spanNode.innerHTML = "Clicked Add: " + d.toISOString();
+    paragraphNode.appendChild(spanNode)
 
     let buttonNode = document.createElement('button')
     buttonNode.innerHTML = 'Delete'
     buttonNode.style.float = 'right'
-
+    buttonNode.classList.add("sizeFont")
     buttonNode.addEventListener("click", () => {
         nListe.removeChild(newNode);
     })
 
     paragraphNode.appendChild(buttonNode)
-    newNode.appendChild(spanNode)
+    newNode.appendChild(paragraphNode)
     nListe.appendChild(newNode);
 }
