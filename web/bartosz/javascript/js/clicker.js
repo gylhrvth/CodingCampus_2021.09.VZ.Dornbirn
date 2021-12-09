86d78c5d5f82012158c27688a3ee465050b96066
Bartosz <B.Michalak@eclipso.at>
2021-12-09 08:31:21 +0100

    Web

--- web/bartosz/javascript/js/clicker.js
+++ web/bartosz/javascript/js/clicker.js
@@ -1,13 +1,10 @@
 let clicks = 0;
 
 function increase ( ) {
-    clicks += 1;
+    clicks ++;
     document.getElementById("counterText").innerHTML = clicks;
     localStorage.setItem("Clicks", clicks);
 }
-console.log(localStorage.getItem("Clicks"));
-
 let ctn = localStorage.getItem("Clicks");
-console.log(ctn);
 
 document.getElementById("local").innerHTML = ctn;
