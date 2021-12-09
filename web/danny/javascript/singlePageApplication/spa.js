6be8743187b92e3394267c2a58bf95260b0fdb99
DanGO <dannygoerke@icloud,com>
2021-12-09 13:45:25 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	CodingCampus/src/Rauan/bank/DatabaseManager.java

--- web/danny/javascript/singlePageApplication/spa.js
+++ web/danny/javascript/singlePageApplication/spa.js
@@ -13,12 +13,33 @@ async function randomJoke() {
     } catch (err) {
         document.getElementById("jokeValue").innerHTML = err.message;
     }
+}
+
+async function searchJokeByText() {
+    let query = "God";
+    try {
+        let response = await fetch("https://api.chucknorris.io/jokes/search?query=God")
+        if (response.ok) {
+            let data = await response.json();
+            document.getElementById("jokeText").innerHTML = data["value"];
+        } else {
+            document.getElementById("jokeText").innerHTML = "Request war nicht erfolgreich. Statuscode: " + response.status + " " + response.statusText;
+        }
+    } catch (err) {
+        document.getElementById("jokeText").innerHTML = err.message;
+
+    }
+}
 
 
-    /**fetch("https://api.chucknorris.io/jokes/random")
-        .then(response => response.json())
-        .then(data => document.getElementById("jokeValue").innerHTML = data["value"])
-        .catch(err => document.getElementById("jokeValue").innerHTML = err.message);
+
+
+
+
+/**fetch("https://api.chucknorris.io/jokes/random")
+    .then(response => response.json())
+    .then(data => document.getElementById("jokeValue").innerHTML = data["value"])
+    .catch(err => document.getElementById("jokeValue").innerHTML = err.message);
 **/
     // joke = response
 
@@ -26,7 +47,7 @@ async function randomJoke() {
 
 
 
-}
+
 
 
 
